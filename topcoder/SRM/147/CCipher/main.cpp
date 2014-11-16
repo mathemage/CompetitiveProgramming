5cfab349c804536ffa6e3186a7ead0fb0c382f19
kha <karel.ha@cern.ch>
2014-11-16 14:41:56 +0100

    Fix SRM 147, Div 2, Problem CCipher
    - previously without system test checks
    - plus length of alphabet, in case of negative shifted index

--- topcoder/SRM/147/CCipher/main.cpp
+++ topcoder/SRM/147/CCipher/main.cpp
@@ -1,14 +1,19 @@
+#include<cassert>
 #include<string>
 using namespace std;
 
 class CCipher {
 public:
-	string decode(string cipherText, int shift) {
-		string orig;
-		for (char c : cipherText) {
-			orig += 'A' + (c - 'A' - shift) % 26 ;
-			//orig.append();
-		}
-		return orig;
-	}
+  string decode(string cipherText, int shift) {
+    string orig;
+    for (char c : cipherText) {
+      orig += 'A' + (c - 'A' - shift + 26) % 26 ;
+    }
+    return orig;
+  }
 };
+
+int main() {
+  CCipher cc;
+  assert(cc.decode("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 10) == "QRSTUVWXYZABCDEFGHIJKLMNOP");
+}
