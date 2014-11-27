ec7031d9da96d54006652674919b2f419294b9fc
Karel Ha <mathemage@gmail.com>
2014-11-27 15:22:13 +0100

    Solve SRM 159, Div 2, Problem StreetParking

---
+++ topcoder/SRM/159/250/StreetParking.cpp
@@ -0,0 +1,167 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class StreetParking {
+public:
+	int freeParks(string);
+};
+
+int StreetParking::freeParks(string street) {
+	int sz = street.size();
+	vector<bool> free(sz);
+	
+	for (int i = 0; i < sz; i++) {
+		switch (street[i]) {
+		case 'D':
+			free[i] = true;
+			break;
+		case 'B':
+			free[i] = true;
+			if (i >= 1) free[i-1] = true;
+			if (i >= 2) free[i-2] = true;
+			break;
+		case 'S':
+			free[i] = true;
+			if (i >= 1) free[i-1] = true;
+			if (i < sz - 1) free[i+1] = true;
+			break;
+		}
+	}
+	return sz - accumulate(free.begin(), free.end(), 0);
+}
+
+
+double test0() {
+	string p0 = "---B--S-D--S--";
+	StreetParking * obj = new StreetParking();
+	clock_t start = clock();
+	int my_answer = obj->freeParks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "DDBDDBDDBDD";
+	StreetParking * obj = new StreetParking();
+	clock_t start = clock();
+	int my_answer = obj->freeParks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "--S--S--S--S--";
+	StreetParking * obj = new StreetParking();
+	clock_t start = clock();
+	int my_answer = obj->freeParks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 2;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "SSD-B---BD-DDSB-----S-S--------S-B----BSB-S--B-S-D";
+	StreetParking * obj = new StreetParking();
+	clock_t start = clock();
+	int my_answer = obj->freeParks(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 14;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
