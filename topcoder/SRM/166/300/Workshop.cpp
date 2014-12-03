6ee8a5f97624ceddb6e1f15df600bfecff0f1e61
Karel Ha <mathemage@gmail.com>
2014-12-03 21:44:03 +0100

    Solve SRM 166, Div 2, Problem Workshop

---
+++ topcoder/SRM/166/300/Workshop.cpp
@@ -0,0 +1,194 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Workshop {
+public:
+	int pictureFrames(vector <int>);
+};
+
+int Workshop::pictureFrames(vector <int> pieces) {
+	int sz = pieces.size();
+	int a, b, c, res = 0;
+	for (int i = 0; i < sz; i++) {
+		for (int j = i+1; j < sz; j++) {
+			for (int k = j+1; k < sz; k++) {
+				a = min(pieces[i], pieces[j]);
+				a = min(a, pieces[k]);
+				
+				c = max(pieces[i], pieces[j]);
+				c = max(c, pieces[k]);
+				
+				b = pieces[i]+pieces[j]+pieces[k]-a-c;
+				res += a+b > c;
+			}
+		}
+	}
+	return res;
+}
+
+
+double test0() {
+	int t0[] = {1,2,3,4,5};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	Workshop * obj = new Workshop();
+	clock_t start = clock();
+	int my_answer = obj->pictureFrames(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {8,5,3};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	Workshop * obj = new Workshop();
+	clock_t start = clock();
+	int my_answer = obj->pictureFrames(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {4,23,76,22,87,3,1,99};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	Workshop * obj = new Workshop();
+	clock_t start = clock();
+	int my_answer = obj->pictureFrames(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 7;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int t0[] = {10000,9999,9998,9997,9996,1,2,3,4,5};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	Workshop * obj = new Workshop();
+	clock_t start = clock();
+	int my_answer = obj->pictureFrames(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 43;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test4() {
+	int t0[] = {100};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	Workshop * obj = new Workshop();
+	clock_t start = clock();
+	int my_answer = obj->pictureFrames(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	time = test4();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
