50469d387c733dd03f0bfd54a698f127753cfdaf
Karel Ha <mathemage@gmail.com>
2014-12-04 23:02:31 +0100

    Solve SRM 167, Div 2, Problem EyeDrops
    - beware of default integer rounding in divisions, when saving to
      double!!

---
+++ topcoder/SRM/167/300/EyeDrops.cpp
@@ -0,0 +1,158 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+#define MSG(a) cout << #a << " == " << a << endl;
+
+class EyeDrops {
+public:
+	double closest(int, int);
+};
+
+double EyeDrops::closest(int s, int k) {
+	double t = 1440.0;
+	t /= k;
+	if (t >= 60*s) {
+		return t;
+	} else {
+		return (1440.0-60*s)/(k-1);
+	}
+}
+
+
+double test0() {
+	int p0 = 8;
+	int p1 = 2;
+	EyeDrops * obj = new EyeDrops();
+	clock_t start = clock();
+	double my_answer = obj->closest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	double p2 = 720.0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int p0 = 9;
+	int p1 = 3;
+	EyeDrops * obj = new EyeDrops();
+	clock_t start = clock();
+	double my_answer = obj->closest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	double p2 = 450.0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int p0 = 23;
+	int p1 = 1;
+	EyeDrops * obj = new EyeDrops();
+	clock_t start = clock();
+	double my_answer = obj->closest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	double p2 = 1440.0;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	int p0 = 9;
+	int p1 = 8;
+	EyeDrops * obj = new EyeDrops();
+	clock_t start = clock();
+	double my_answer = obj->closest(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	double p2 = 128.57142857142856;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
