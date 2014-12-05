1160bc6b33a0da016bfc137a7c8dcffb88e27a6c
Karel Ha <mathemage@gmail.com>
2014-12-05 13:16:21 +0100

    Solve SRM 168, Div 2, Problem StairClimb

---
+++ topcoder/SRM/168/250/StairClimb.cpp
@@ -0,0 +1,131 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class StairClimb {
+public:
+	int stridesTaken(vector <int>, int);
+};
+
+int StairClimb::stridesTaken(vector <int> flights, int s) {
+	int t = 0;
+	for (int f : flights) {
+		t += 2 + (f+s-1)/s;
+	}
+	return t-2;
+}
+
+
+double test0() {
+	int t0[] = {15};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 2;
+	StairClimb * obj = new StairClimb();
+	clock_t start = clock();
+	int my_answer = obj->stridesTaken(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 8;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	int t0[] = {15,15};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 2;
+	StairClimb * obj = new StairClimb();
+	clock_t start = clock();
+	int my_answer = obj->stridesTaken(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 18;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	int t0[] = {5,11,9,13,8,30,14};
+	vector <int> p0(t0, t0+sizeof(t0)/sizeof(int));
+	int p1 = 3;
+	StairClimb * obj = new StairClimb();
+	clock_t start = clock();
+	int my_answer = obj->stridesTaken(p0, p1);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p2 = 44;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p2 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p2 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+//Powered by [KawigiEdit] 2.0!
