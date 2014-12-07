53a7cdf2e924c810985bb22eb90b385eeacf39c2
kha <karel.ha@cern.ch>
2014-12-07 22:35:52 +0100

    Solve SRM 168, Div 2, Problem NumberGuesser

---
+++ topcoder/SRM/168/500/NumberGuesser.cpp
@@ -0,0 +1,178 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+#define MSG(a) cout << #a << " == " << a << endl;
+
+class NumberGuesser {
+public:
+	string digits(int n);
+	int guess(string);
+};
+
+string NumberGuesser::digits(int n) {
+	char c;
+	string r;
+	while (n) {
+		c = '0' + n % 10;
+		if (c != '0') r += c;
+		n /= 10;
+	}
+	sort(r.begin(), r.end());
+	return r;
+}
+
+int NumberGuesser::guess(string leftOver) {
+	for (char d = '1'; d <= '9'; d++) {
+		for (int pos = 0; pos < 4; pos++) {	
+			string diff = leftOver;
+  		
+  		string dig(1, d);
+			diff.insert(pos, dig);
+			int c = stoi(diff);
+			
+			// test out: a - b = c
+			int a;
+			for (int b = 1; b < 9999; b++) {
+				a = b + c;
+				if (digits(a) == digits(b)) return d - '0';		 
+			}
+		}
+	}
+}
+
+
+double test0() {
+	string p0 = "087";
+	NumberGuesser * obj = new NumberGuesser();
+	clock_t start = clock();
+	int my_answer = obj->guess(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 3;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test1() {
+	string p0 = "099";
+	NumberGuesser * obj = new NumberGuesser();
+	clock_t start = clock();
+	int my_answer = obj->guess(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 9;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test2() {
+	string p0 = "191";
+	NumberGuesser * obj = new NumberGuesser();
+	clock_t start = clock();
+	int my_answer = obj->guess(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 7;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+double test3() {
+	string p0 = "689";
+	NumberGuesser * obj = new NumberGuesser();
+	clock_t start = clock();
+	int my_answer = obj->guess(p0);
+	clock_t end = clock();
+	delete obj;
+	cout <<"Time: " <<(double)(end-start)/CLOCKS_PER_SEC <<" seconds" <<endl;
+	int p1 = 4;
+	cout <<"Desired answer: " <<endl;
+	cout <<"\t" << p1 <<endl;
+	cout <<"Your answer: " <<endl;
+	cout <<"\t" << my_answer <<endl;
+	if (p1 != my_answer) {
+		cout <<"DOESN'T MATCH!!!!" <<endl <<endl;
+		return -1;
+	}
+	else {
+		cout <<"Match :-)" <<endl <<endl;
+		return (double)(end-start)/CLOCKS_PER_SEC;
+	}
+}
+
+int main() {
+	int time;
+	bool errors = false;
+	
+	time = test0();
+	if (time < 0)
+		errors = true;
+	
+	time = test1();
+	if (time < 0)
+		errors = true;
+	
+	time = test2();
+	if (time < 0)
+		errors = true;
+	
+	time = test3();
+	if (time < 0)
+		errors = true;
+	
+	if (!errors)
+		cout <<"You're a stud (at least on the example cases)!" <<endl;
+	else
+		cout <<"Some of the test cases had errors." <<endl;
+}
+
+
+//Powered by [KawigiEdit] 2.0!
