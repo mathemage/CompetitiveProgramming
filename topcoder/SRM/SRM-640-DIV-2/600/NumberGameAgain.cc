d553a2a555a27b93a6adb9278dddcb52effe3a46
kha <karel.ha@cern.ch>
2014-12-13 23:57:35 +0100

    Solve SRM 600, Div 2, Problem NumberGameAgain
    - 180.01 pts
    - "1 << k" should be written as "1LL << k" or "pow(2,k)"

---
+++ topcoder/SRM/SRM-640-DIV-2/600/NumberGameAgain.cc
@@ -0,0 +1,71 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+
+using namespace std;
+
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define MSG(a) cout << #a << " == " << a << endl;
+
+typedef vector<bool> vb;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef long long ll;
+
+class NumberGameAgain {
+public:
+  long long solve(int k, vector<long long> table) {
+    ll result = pow(2,k)-2;
+    sort(table.begin(), table.end());
+
+    int sz = table.size();
+    vector<bool> invalid(sz, false);
+    vector<int> width(sz);
+
+    for (int i = 0; i < sz; i++) {
+      ll t = table[i];
+      while (t) {
+        t >>= 1;
+        width[i]++;
+      }	
+    }
+
+    for (int i = 0; i < sz-1; i++) {
+      if (invalid[i])
+        continue;
+      for (int j = i+1; j < sz; j++) {
+        int shift = width[j]-width[i];
+        if ( table[i] == (table[j] >> shift) ) {
+          invalid[j] = true;
+        }
+      }
+    }
+
+    for (int i = 0; i < sz; i++) {
+      if (!invalid[i]) {
+        result -= pow(2, k-width[i]+1) - 1;
+      }
+    }
+
+    return result;
+  }
+};
