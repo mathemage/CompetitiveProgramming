a3301431e8d34b19a0ea7f3867fcc2c6290ae12c
kha <karel.ha@cern.ch>
2015-01-01 17:37:04 +0100

    Include Problem statement for gdocs/circus

--- gdocs/Circus/circus.cpp
+++ gdocs/Circus/circus.cpp
@@ -13,9 +13,13 @@ LANG: C++11
 
  * Created By : Karel Ha <mathemage@gmail.com>
 
- * URL :
-
- * Points Gained (in case of online contest) :
+ * Problem statement :
+ *
+ * A circus is designing a tower routine consisting of people standing atop one
+ * another's shoulders. For practical and aesthetic reasons, each person must
+ * be both shorter and lighter than the person below him or her. Given the
+ * heights and weights of each person in the circus, write a method to compute
+ * the largest possible number of people in such a tower.
 
  ==========================================*/
 
