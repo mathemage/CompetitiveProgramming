df1f0e613cc5c679952d88127fa5e839d7a9371c
kha <karel.ha@cern.ch>
2015-01-06 20:54:34 +0100

    Solve SRM 183, Div 2, Problem CountGame

---
+++ topcoder/SRM/SRM-183-DIV-2/350/CountGame.cc
@@ -0,0 +1,42 @@
+/* ========================================
+ * Points : 131.37
+ * Total : 350
+ * Status : ac on 1st attempt
+ ==========================================*/
+
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define MSG(a) cout << #a << " == " << a << endl;
+
+class CountGame {
+public:
+  int howMany(int maxAdd, int goal, int next) {
+    int res = (goal - next + 1) % (maxAdd + 1);
+    return (res == 0) ? -1 : res;
+  }
+};
