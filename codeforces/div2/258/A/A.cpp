e1f076702678945700e7343935309623b6b13ee4
Karel Ha <mathemage@gmail.com>
2015-08-25 16:58:34 +0200

    Codeforces #258, Div 2, Problem A

---
+++ codeforces/div2/258/A/A.cpp
@@ -0,0 +1,53 @@
+ 
+/* ========================================
+
+   * File Name : A.cpp
+
+   * Creation Date : 25-08-2015
+
+   * Last Modified : Tue 25 Aug 2015 04:58:14 PM CEST
+
+   * Created By : Karel Ha <mathemage@gmail.com>
+
+   * URL : http://codeforces.com/problemset/problem/451/A
+
+   * Points Gained (in case of online contest) : AC
+
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(I,N)   FOR(I,0,N)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define ALL(A)     (A).begin(), (A).end()
+template <typename T>
+string NumberToString ( T Number ) {
+  ostringstream ss;
+  ss << Number;
+  return ss.str();
+}
+
+#define ERR(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+vector<string> split(const string& s, char c) {
+  vector<string> v;
+  stringstream ss(s);
+  string x;
+  while (getline(ss, x, c))
+    v.emplace_back(x);
+  return move(v);
+}
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>
+void err(vector<string>::iterator it, T a, Args... args) {
+  cout << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << endl;
+  err(++it, args...);
+}
+
+int main() {
+  int n,m;
+  cin >> n >> m;
+  cout << ((min(n,m) % 2) ? "Akshat" : "Malvika") << endl;
+  return 0;
+}
