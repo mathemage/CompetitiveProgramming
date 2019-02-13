b01197545b65fa95f89d4d6da6f6e74cdc927071
Karel Ha <mathemage@gmail.com>
2019-02-13 13:30:00 +0100

    Create solution for SPOJ palin
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- spoj/00005/palin.py
+++ spoj/00005/palin.py
@@ -1,2 +1,38 @@
-k = int(input())
-# print(k)
+#!/usr/bin/env python3
+def next_palindrome(k):
+	palin = list(k)
+	n = len(k)
+	mid = n // 2
+	
+	# case 1: forward right
+	just_copy = False
+	for i in range(mid, n):
+		mirrored = n - 1 - i
+		if k[i] < k[mirrored]:
+			just_copy = True
+		if just_copy:
+			palin[i] = palin[mirrored]
+	
+	# case 2: backward left
+	if not just_copy:
+		i = (n - 1) // 2    # TODO debug here
+		while i >= 0 and k[i] != '9':
+			i -= 1
+		if i >= 0:
+			palin[i] = str(int(k[i]) + 1)
+			for j in range(i + 1, mid + 1):
+				palin[j] = '0'
+			for j in range(mid + 1, n):
+				mirrored = n - 1 - j
+				palin[j] = palin[mirrored]
+		else:
+			raise NotImplemented
+	
+	return ''.join(palin)
+
+
+if __name__ == '__main__':
+	t = int(input())
+	for _ in range(t):
+		k = input()
+		print(next_palindrome(k))
