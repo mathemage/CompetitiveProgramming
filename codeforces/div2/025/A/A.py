62652097b369ae759e481057d87aa4c4bf16a0e4
Karel Ha <mathemage@gmail.com>
2019-02-22 15:20:43 +0100

    Output index, not the value
    - in CF Div 2 25A
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div2/025/A/A.py
+++ codeforces/div2/025/A/A.py
@@ -1,7 +1,7 @@
 n = int(input())
 ns = list(map(int, input().split()))
 
-xs = [k for k in ns if k % 2 == 0]
-ys = [k for k in ns if k % 2 != 0]
+xs = [i for i, k in enumerate(ns) if k % 2 == 0]
+ys = [i for i, k in enumerate(ns) if k % 2 != 0]
 zs = xs if len(xs) == 1 else ys
-print(zs[0])
+print(zs[0] + 1)
