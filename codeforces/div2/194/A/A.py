afc738f3621f2ff6201b8ef09a80bd5d77ac5df3
Karel Ha <mathemage@gmail.com>
2019-03-10 15:42:37 +0100

    Fix: greedy solution
    - in CF Div 2 194A
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div2/194/A/A.py
+++ codeforces/div2/194/A/A.py
@@ -1,5 +1,20 @@
 n = int(input())
 
 s = n * (n ** 2 + 1) // 2
+used = [False] * n ** 2
 for i in range(1, n + 1):
-	print("{} {}".format(i, s - i))
+	current_s = s
+	current_bag = []
+	k = n ** 2
+
+	while current_s != 0:
+		if current_s <= n ** 2 and not used[current_s-1]:
+			k = current_s
+		if not used[k-1]:
+			current_bag.append(k)
+			used[k-1] = True
+			current_s -= k
+		else:
+			k -= 1
+
+	print(' '.join(map(str, current_bag)))
