3646e1740a70482dbb7d010de7298c2cca3580cd
Karel Ha <mathemage@gmail.com>
2019-03-21 17:33:38 +0100

    Fix: solve in O(n) using watermark
    - in CF Div 2 548B
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div2/548/B/B.py
+++ codeforces/div2/548/B/B.py
@@ -1,8 +1,12 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-for i in reversed(range(1, n)):
-	for j in range(i):
-		a[j] = min(a[j], a[i] - 1 if a[i] > 0 else 0)
+watermark = a[-1]
+for i in reversed(range(n - 1)):
+	if a[i] < watermark:
+		watermark = a[i]
+	else:
+		a[i] = max(watermark - 1, 0)
+		watermark = a[i]
 
 print(sum(a))
