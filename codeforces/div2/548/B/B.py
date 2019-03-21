569d30b8ce26010de459682b56b0673b5d572f9d
Karel Ha <mathemage@gmail.com>
2019-03-21 17:27:34 +0100

    Fix: reverse order
    - in CF Div 2 548B
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div2/548/B/B.py
+++ codeforces/div2/548/B/B.py
@@ -1,7 +1,7 @@
 n = int(input())
 a = list(map(int, input().split()))
 
-for i in range(1, n):
+for i in reversed(range(1, n)):
 	for j in range(i):
 		a[j] = min(a[j], a[i] - 1 if a[i] > 0 else 0)
 
