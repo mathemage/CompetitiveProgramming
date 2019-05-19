0461aff0529c73484c91053737b60b1ab05c580c
Karel Ha <mathemage@gmail.com>
2019-05-19 18:05:36 +0200

    Solve
    - in CF Div 2 69A
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/069/A/A.py
@@ -0,0 +1,6 @@
+n, m = map(int, input().split())
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+ni = primes.index(n) if n in primes else None
+mi = primes.index(m) if m in primes else None
+print("YES" if mi and ni and mi == ni + 1 else "NO")
