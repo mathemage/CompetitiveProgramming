0f02217f1dc80a8ba189a3877705ce5f335abf4d
Karel Ha <mathemage@gmail.com>
2019-05-19 18:10:08 +0200

    Fix: not None bug
    - in CF Div 2 69A
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div2/069/A/A.py
+++ codeforces/div2/069/A/A.py
@@ -3,4 +3,4 @@ primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
 ni = primes.index(n) if n in primes else None
 mi = primes.index(m) if m in primes else None
-print("YES" if mi and ni and mi == ni + 1 else "NO")
+print("YES" if mi is not None and ni is not None and mi == ni + 1 else "NO")
