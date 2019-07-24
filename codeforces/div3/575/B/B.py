37daed98ee3e6f835276a8a8de9e963f997c1881
Karel Ha <mathemage@gmail.com>
2019-07-24 17:40:08 +0200

    Fix: logic for odds_in_last_segment
    - in CF Div 3 575B
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div3/575/B/B.py
+++ codeforces/div3/575/B/B.py
@@ -10,13 +10,13 @@ for _ in range(q):
         if x % 2 == 1:  # odd
             if len(r) < k:
                 r.append(i + 1)
-            else:
+
+            if len(r) == k:
+                r[-1] = i + 1
                 odds_in_last_segment += 1
 
-    if len(r) == k - 1 and odds_in_last_segment % 2 == 1:
-        r.append(n)
-
-    if len(r) == k:
+    if len(r) == k and odds_in_last_segment % 2 == 1:
+        r[-1] = n
         print("YES")
         print(' '.join(map(str, r)))
     else:
