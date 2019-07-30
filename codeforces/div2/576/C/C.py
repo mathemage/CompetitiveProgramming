b9d4baadb9907639a4ba93116542bdd5b94fd57b
Karel Ha <mathemage@gmail.com>
2019-07-30 18:23:13 +0200

    Fix: interval of K values, not K most frequent values
    - in CF Div 2 576C
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div2/576/C/C.py
+++ codeforces/div2/576/C/C.py
@@ -15,5 +15,15 @@ for ai in a:
         values[ai] = 0
     values[ai] += 1
 
-sorted_values = sorted(values.items(), key=lambda kv: kv[1], reverse=True)
-print(n - sum([v for _, v in sorted_values[:K]]))
+sorted_values = sorted(values.items())
+
+K_sum = sum([v for _, v in sorted_values[:K]])
+max_K_sum = K_sum
+for i in range(K, len(sorted_values)):
+    decr = sorted_values[i-K][1]
+    incr = sorted_values[i][1]
+    K_sum -= decr
+    K_sum += incr
+    max_K_sum = max(max_K_sum, K_sum)
+
+print(n - max_K_sum)
