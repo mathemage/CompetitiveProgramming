6243265b5fd32c9135730abe0117b2e9ea36d6a9
Karel Ha <mathemage@gmail.com>
2019-08-13 19:02:59 +0200

    Fix RTE 5: lazy range, not explicit list (mem err)
    - in CF Div 3 579C
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div3/579/C/C.py
+++ codeforces/div3/579/C/C.py
@@ -9,14 +9,22 @@ for ai in a[1:]:
     divisor = gcd(divisor, ai)
 
 result = 1      # 1 is always a divisor
-limit = divisor // 2 + 1
-primes = [2] + list(range(3, limit, 2))
-for prime_factor in primes:
+limit = int(divisor // 2 + 1)
+
+# 2
+prime_factor = 2
+power = 0
+while divisor % prime_factor == 0:
+    divisor //= prime_factor
+    power += 1
+result *= 1 + power
+
+for prime_factor in range(3, limit, 2):
     if prime_factor > divisor:
         break
     power = 0
     while divisor % prime_factor == 0:
-        divisor /= prime_factor
+        divisor //= prime_factor
         power += 1
     result *= 1 + power
 
