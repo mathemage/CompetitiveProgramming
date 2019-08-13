5a79acaccab59e50788cd2da31a93f2f1b69ca53
Karel Ha <mathemage@gmail.com>
2019-08-13 18:44:28 +0200

    Fix WA 10: remove limit heuristics
    - in CF Div 3 579C
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- codeforces/div3/579/C/C.py
+++ codeforces/div3/579/C/C.py
@@ -9,14 +9,15 @@ for ai in a[1:]:
     divisor = gcd(divisor, ai)
 
 result = 1      # 1 is always a divisor
-limit = ceil(sqrt(divisor)) + 1
+limit = divisor // 2 + 1
 primes = [2] + list(range(3, limit, 2))
 for prime_factor in primes:
+    if prime_factor > divisor:
+        break
     power = 0
     while divisor % prime_factor == 0:
         divisor /= prime_factor
         power += 1
-    if power > 0:
-        result *= 1 + power
+    result *= 1 + power
 
 print(result)
