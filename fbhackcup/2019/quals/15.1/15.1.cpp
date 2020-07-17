b8ac9e88ebfbbcc6efaa182b200a16f8d61f8022
Karel Ha <mathemage@gmail.com>
2020-07-17 13:12:16 +0200

    Create 15.1.cpp
    - Fb Hackercup '19 Quals, 15.1
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ fbhackcup/2019/quals/15.1/15.1.cpp
@@ -0,0 +1,57 @@
+ 
+/* ========================================
+
+   * File Name : 15.1.cpp
+
+   * Creation Date : 17-07-2020
+
+   * Last Modified : Pá 17. července 2020, 13:11:36
+
+   * Created By : Karel Ha <mathemage@gmail.com>
+
+   * URL : https://www.facebook.com/codingcompetitions/hacker-cup/2019/qualification-round/problems/A
+
+   * Points Gained (in case of online contest) :
+
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(I,N)   FOR(I,0,N)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define ALL(A)     (A).begin(), (A).end()
+#define MSG(a) cout << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+
+template <typename T>
+string NumberToString ( T Number ) {
+  ostringstream ss;
+  ss << Number;
+  return ss.str();
+}
+
+#define ERR(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+vector<string> split(const string& s, char c) {
+  vector<string> v;
+  stringstream ss(s);
+  string x;
+  while (getline(ss, x, c))
+    v.emplace_back(x);
+  return move(v);
+}
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>
+void err(vector<string>::iterator it, T a, Args... args) {
+  cout << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << endl;
+  err(++it, args...);
+}
+
+int main() {
+  string result = "mock output";
+  MSG(result)
+
+  return 0;
+}
