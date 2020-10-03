e76deb7ef6764c652fafaeadbe7710a46493df99
Karel Ha <mathemage@gmail.com>
2020-10-03 21:18:54 +0100

    Codeforces #304, Div 2, Problem A
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/546/A/A.cpp
@@ -0,0 +1,60 @@
+ 
+/* ========================================
+
+   * File Name : A.cpp
+
+   * Creation Date : 03-10-2020
+
+   * Last Modified : So 3. října 2020, 21:25:26
+
+   * Created By : Karel Ha <mathemage@gmail.com>
+
+   * URL : https://codeforces.com/problemset/problem/546/A
+
+   * Points Gained (in case of online contest) : AC
+
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(I,N)   FOR(I,0,N)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define ALL(A)     (A).begin(), (A).end()
+#define MSG(a) cout << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+
+template <typename T>
+string NumberToString ( T Number ) {
+  ostringstream ss;
+  ss << Number;
+  return ss.str();
+}
+
+#define ERR(args...) { vector<string> _v = split(#args, ','); err(_v.begin(), args); }
+vector<string> split(const string& s, char c) {
+  vector<string> v;
+  stringstream ss(s);
+  string x;
+  while (getline(ss, x, c))
+    v.emplace_back(x);
+  return move(v);
+}
+void err(vector<string>::iterator it) {}
+template<typename T, typename... Args>
+void err(vector<string>::iterator it, T a, Args... args) {
+  cout << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << endl;
+  err(++it, args...);
+}
+
+int main() {
+  int k, n, w;
+  cin >> k >> n >> w;
+
+  int needed = w * (w + 1) / 2 * k;
+//   MSG(needed)
+  cout << max(0, needed - n);
+  return 0;
+}
