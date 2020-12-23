23d575285c6f9d84ab2be3e93c2be37246dc02fa
Karel Ha <mathemage@gmail.com>
2020-12-23 20:18:58 +0100

    Coding kata: TC SRM-796-DIV-2-250 (ChristmasCrackerHappiness)
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/SRM-796-DIV-2/250/ChristmasCrackerHappiness.cc
@@ -0,0 +1,26 @@
+/* ========================================
+ * Points :
+ * Total :
+ * Status :
+ ==========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define FOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define MSG(a) cerr << #a << " == " << a << endl;
+
+const int CLEAN = -1;
+
+
+class ChristmasCrackerHappiness {
+public:
+  int solve(int N, vector <int> winner, vector <int> loser) {
+    int result;
+
+    return result;
+  }
+};
