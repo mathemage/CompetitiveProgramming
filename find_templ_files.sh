fcfc6088bc109049f934fb90d4aef7ba2610f31e
Karel Ha <mathemage@gmail.com>
2021-01-20 00:21:13 +0100

    Create find_templ_files.sh
    To have list of template files to open with Vim
    - see https://vim.fandom.com/wiki/Open_file_under_cursor
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ find_templ_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+find . -iname *template*
+
+# ./vim/syntax/cxx.template
+# ./vim/syntax/cpp.template
+# ./vim/syntax/template.vim
+# ./codeforces/TEMPLATE-round
+# ./topcoder/SRM/C++Template
