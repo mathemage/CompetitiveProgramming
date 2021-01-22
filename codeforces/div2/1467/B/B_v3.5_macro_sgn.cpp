037bb4c62598da023dde97419ce87e0dd07dfc9a
Karel Ha <mathemage@gmail.com>
2021-01-22 19:58:48 +0100

    Upsolve Codeforces #695B, Div 2
    Use macro SGN
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/1467/B/B_v3.5_macro_sgn.cpp
@@ -0,0 +1,85 @@
+ 
+/* ========================================
+   * File Name : B_v3.cpp
+   * Creation Date : 17-01-2021
+   * Last Modified : Pá 22. ledna 2021, 19:58:40
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/contest/1467/problem/B
+   * Points/Time : 
+   * Total :
+   * Status : AC
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define FO(I,A,B) for(int I = (A); I <= (B); ++I)
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define RFOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define MSG(a) cerr << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+
+int sign(int x) {
+  return SGN(x);
+}
+
+bool is_extreme(int l, int m, int r) {
+  return sign(l-m) * sign(r-m) > 0;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  vector<bool> extremes(n);
+  REP(i,n) {
+    cin >> a[i];
+  }
+
+  FO(j,1,n-2) {
+    extremes[j] = is_extreme(a[j-1], a[j], a[j+1]);
+  }
+
+  int max_delta = 0;
+  FO(j,1,n-2) {
+    for (auto new_val: {a[j-1], a[j+1]}) {
+      int delta = extremes[j-1] + extremes[j] + extremes[j+1];
+      swap(a[j], new_val);
+      for (auto & dj: {-1, 0, 1}) {
+        int jj = j+dj;
+        if (0<jj && jj<n-1) {
+          delta -= is_extreme(a[jj-1], a[jj], a[jj+1]);
+        }
+      }
+      MAXUPDATE(max_delta, delta);
+      swap(a[j], new_val);
+    }
+  }
+
+  long long result = accumulate(ALL(extremes), 0) - max_delta;
+  cout << result << endl;
+}
+
+int main() {
+  int cases;
+  cin >> cases;
+//   cases = 1;
+  while (cases--) {
+    solve();
+  }
+  return 0;
+}
