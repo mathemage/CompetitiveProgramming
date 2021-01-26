e3208114c3cabd5c062121432a67fd8a332cb089
Karel Ha <mathemage@gmail.com>
2021-01-26 20:56:20 +0100

    Quora Haqathon - Upvotes
    Brute force solution (TLEs)
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- quora/Quora-Haqathon/upvotes/upvotes_via_brute_force.cpp
+++ quora/Quora-Haqathon/upvotes/upvotes_via_brute_force.cpp
@@ -2,12 +2,12 @@
 /* ========================================
    * File Name : upvotes.cpp
    * Creation Date : 22-01-2021
-   * Last Modified : Út 26. ledna 2021, 20:50:51
+   * Last Modified : Út 26. ledna 2021, 20:58:04
    * Created By : Karel Ha <mathemage@gmail.com>
    * URL : https://www.hackerrank.com/contests/quora-haqathon/challenges/upvotes
-   * Points/Time : (57m 24 s + )
+   * Points/Time : (57m 20 s + 33 m 50 s) = 1h 31 m 10 s
    * Total :
-   * Status :
+   * Status : AC 9/29 & TLE (rest)
    ==========================================*/
 
 #include <bits/stdc++.h>
@@ -46,12 +46,12 @@ void solve_via_brute_force() {
     if (i>0) {
       signs[i] = SGN(upvotes[i]-upvotes[i-1]);
     }
-//     MSG(upvotes[i]); MSG(signs[i]); cerr << endl;
-    cerr << signs[i] << " ";
+//     MSG(upvotes[i]); cerr << endl;
+//     cerr << signs[i] << " ";
   }
 
   for (int win_start=0, win_end=win_start+K-1 ; win_end < N; win_start++, win_end++) {
-    cerr << endl << endl; MSG(win_start); MSG(win_end);
+//     cerr << endl << endl; MSG(win_start); MSG(win_end);
     long long result = 0LL;
     FO(start,win_start,win_end) FO(end,start+1,win_end) {
       int min_sign = INF;
@@ -61,60 +61,12 @@ void solve_via_brute_force() {
         MAXUPDATE(max_sign, signs[i]);
       }
 
-      if (min_sign == 0 && max_sign == 1) { result++; }
-      if (min_sign == -1 && max_sign == 0) { result--; }
-      cerr << endl; MSG(start); MSG(end); MSG(min_sign); MSG(max_sign); MSG(result);
+      if (min_sign >= 0 && max_sign == 1) { result++; }
+      if (min_sign == -1 && max_sign <= 0) { result--; }
+//       cerr << endl; MSG(start); MSG(end); MSG(min_sign); MSG(max_sign); MSG(result);
     }
     cout << result << endl;
   }
-}
-
-void solve() {
-  int N, K;
-  cin >> N >> K;
-
-  vector<int> upvotes(N);
-  vector<int> signs(N, UNDEF);
-  REP(i,N) {
-    cin >> upvotes[i];
-    if (i>0) {
-      signs[i] = SGN(upvotes[i]-upvotes[i-1]);
-    }
-//     MSG(upvotes[i]); MSG(signs[i]); cerr << endl;
-  }
-
-  vector<pair<int, int>> non_zero_signs;
-  FO(i,1,N) {
-    if (signs[i] != 0) {
-      non_zero_signs.PB(MP(i, signs[i]));
-    }
-  }
-  cerr << "non_zero_signs: ";
-  for (auto & sg: non_zero_signs) {
-    cerr << sg.F << "(" << sg.S << ")\t";
-  }
-  cerr << endl;
-
-//   // get maximal monotonous intervals
-//   vector<int> left_ends(N, UNDEF);
-//   vector<int> right_ends(N, UNDEF);
-//   FO(i,1,N) {
-//     if (abs(signs[i]) == 1) {
-//       for (auto & di: {-1,1}) {
-//         int pos = i + di;
-//         while (0 < pos && pos < N) {
-// 
-//         }
-//       }
-//     }
-//   }
-
-//   long long result = 0LL;
-//   for (int start = 1; start + k - 1 < signs.size(); start += 1) {
-//     FO(pos,start,start + k - 1) {
-//       result += signs[pos];   // TODO maximal intervals needed here
-//     }
-//   }
 }
 
 int main() {
