864355551d2974c78a52a46012ac172947fb89e3
Karel Ha <mathemage@gmail.com>
2021-01-28 00:44:00 +0100

    Codeforces #697A, Div 3
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/1475/A/A.cpp
@@ -0,0 +1,67 @@
+ 
+/* ========================================
+   * File Name : A.cpp
+   * Creation Date : 28-01-2021
+   * Last Modified : Čt 28. ledna 2021, 00:30:17
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/contest/1475/problem/A
+   * Points/Time : 3m 50s
+   * Total :
+   * Status : AC
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define FO(I,A,B) for(int I = (A); I <= (B); ++I)
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define RFOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define MSG(a) cerr << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  long long n;
+  cin >> n;
+  while (n > 1 && n % 2 == 0) {
+    n /= 2;
+  }
+  cout << (n == 1? "NO": "YES") << endl;
+}
+
+int main() {
+  int cases;
+  cin >> cases;
+//   cases = 1;
+  while (cases--) {
+    solve();
+  }
+  return 0;
+}
