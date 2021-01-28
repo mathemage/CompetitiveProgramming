efa08a10cd742693efa21190daed001bf2f12080
Karel Ha <mathemage@gmail.com>
2021-01-28 18:14:28 +0100

    Quora Haqathon - Upvotes
    Refactor: use struct interval_t instead of {l,r}_end_{pos,sgn}
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- quora/Quora-Haqathon/upvotes/upvotes_via_naive_counting.cpp
+++ quora/Quora-Haqathon/upvotes/upvotes_via_naive_counting.cpp
@@ -2,7 +2,7 @@
 /* ========================================
    * File Name : upvotes.cpp
    * Creation Date : 22-01-2021
-   * Last Modified : Čt 28. ledna 2021, 17:32:32
+   * Last Modified : Čt 28. ledna 2021, 18:13:46
    * Created By : Karel Ha <mathemage@gmail.com>
    * URL : https://www.hackerrank.com/contests/quora-haqathon/challenges/upvotes
    * Points/Time : 1h 31 m 10 s (previous) + ? (~30 m) + 
@@ -34,6 +34,12 @@ const int CLEAN = -1;
 const int UNDEF = INT_MAX;
 const int INF = INT_MAX;
 
+struct interval_t {
+  int L;
+  int R;
+  int sgn;
+};
+
 void solve_via_naive_counting() {
   int N, K;
   cin >> N >> K;
@@ -42,6 +48,7 @@ void solve_via_naive_counting() {
     return;
   }
 
+  //-----values and signs-----
   vector<int> upvotes(N);
   vector<int> signs(N, UNDEF);
   REP(i,N) {
@@ -54,47 +61,74 @@ void solve_via_naive_counting() {
   }
 //   cerr << endl;
 
-  vector<int> l_end_pos, l_end_sgn;
-  vector<int> r_end_pos, r_end_sgn;
-
-  vector<pair<int, int>> pm_sgn;
-  l_end_pos.PB(1); l_end_sgn.PB(signs[1]);
+  //-----maximal monotonous intervals-----
+//   vector<int> l_end_pos, l_end_sgn;
+//   vector<int> r_end_pos, r_end_sgn;
+// 
+//   vector<pair<int, int>> nonzero_pos_sgn;
+//   l_end_pos.PB(1); l_end_sgn.PB(signs[1]);
+//   FO(i,1,N) {
+//     if (signs[i] != 0) {
+//       if (!nonzero_pos_sgn.empty() && nonzero_pos_sgn.back().S == -signs[i]) {
+//         r_end_pos.PB(i-1);
+//         r_end_sgn.PB(nonzero_pos_sgn.back().S);
+// 
+//         l_end_pos.PB(nonzero_pos_sgn.back().F+1);
+//         l_end_sgn.PB(signs[i]);
+//       }
+// 
+//       nonzero_pos_sgn.PB(MP(i, signs[i]));
+//     }
+//   }
+//   r_end_pos.PB(N-1);
+//   r_end_sgn.PB(nonzero_pos_sgn.back().S);
+//
+  vector<interval_t> nonzero_intervals = { {1, 1, signs[1]} };
+  vector<pair<int, int>> nonzero_pos_sgn;
   FO(i,1,N) {
     if (signs[i] != 0) {
-      if (!pm_sgn.empty() && pm_sgn.back().S == -signs[i]) {
-        r_end_pos.PB(i-1);
-        r_end_sgn.PB(pm_sgn.back().S);
-
-        l_end_pos.PB(pm_sgn.back().F+1);
-        l_end_sgn.PB(signs[i]);
+      if (!nonzero_pos_sgn.empty() ) {
+        auto & last_pos_sgn = nonzero_pos_sgn.back();
+        if (last_pos_sgn.S == -signs[i]) {
+          nonzero_intervals.back().R = i-1;
+          nonzero_intervals.PB( {last_pos_sgn.F+1, last_pos_sgn.F+1, signs[i]} );
+        }
       }
 
-      pm_sgn.PB(MP(i, signs[i]));
+      nonzero_pos_sgn.PB(MP(i, signs[i]));
     }
   }
-  r_end_pos.PB(N-1);
-  r_end_sgn.PB(pm_sgn.back().S);
+  nonzero_intervals.back().R = N-1;
 
-//   cerr << "pm_sgn: ";
-//   for (auto & sg: pm_sgn) {
-//     cerr << sg.F << "(" << sg.S << ")\t";
+  //-----debug-print maximal monotonous intervals-----
+  cerr << "nonzero_pos_sgn: ";
+  for (auto & sg: nonzero_pos_sgn) {
+    cerr << sg.F << "(" << sg.S << ")\t";
+  }
+  cerr << endl;
+//   cerr << "lr_end_pos_sgn: ";
+//   for (int j = 0; j < l_end_pos.size(); j += 1) {
+//     cerr << "[" << l_end_pos[j] << ","  << r_end_pos[j] << "]";
+//     cerr << "(" << l_end_sgn[j] << ","  << r_end_sgn[j] << ")\t";
 //   }
 //   cerr << endl;
-  cerr << "lr_end_pos_sgn: ";
-  for (int j = 0; j < l_end_pos.size(); j += 1) {
-    cerr << "[" << l_end_pos[j] << ","  << r_end_pos[j] << "]";
-    cerr << "(" << l_end_sgn[j] << ","  << r_end_sgn[j] << ")\t";
+  cerr << "nonzero_intervals: ";
+  for (auto & interval : nonzero_intervals) {
+    cerr << "[" << interval.L << ","  << interval.R << "]";
+    cerr << "(" << (interval.sgn? (interval.sgn>0?'+':'-') : '0')<< ")\t";
   }
   cerr << endl;
 
-  int win_start = 0, win_end=win_start+K-1;
+  //-----TODO-----
+//   int win_start = 0, win_end=win_start+K-1;
 //   int i_l = 0;
 //   int i_r = 0;
 //   while (! (l_end_pos[i_r] <= win_end && win_end <= r_end_pos[i_r]) ) {
 //     i_r++;  // TODO test
 //   }
 
-  for (win_start=1, win_end=win_start+K-1 ; win_end < N; win_start++, win_end++) {
+//   for (win_start=1, win_end=win_start+K-1 ; win_end < N; win_start++, win_end++) {
+  for (int win_start=0, win_end=win_start+K-1 ; win_end < N; win_start++, win_end++) {
 //     cerr << endl << endl; MSG(win_start); MSG(win_end);
     long long result = 0LL;
     FO(start,win_start,win_end) FO(end,start+1,win_end) {
