29516ac3a7a279e3caac71e75ed2fc623540d833
Karel Ha <mathemage@gmail.com>
2021-02-03 23:53:13 +0100

    Quora Haqathon - Upvotes
    Keep track of `out*_brute_force`
    Update script gd_out.sh
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- quora/Quora-Haqathon/upvotes/gd_out.sh
+++ quora/Quora-Haqathon/upvotes/gd_out.sh
@@ -7,3 +7,8 @@
 ./driver <"in2" &>out2_naive_counting && git diff out2_naive_counting
 ./driver <"in3" &>out3_naive_counting && git diff out3_naive_counting
 ./driver <"in4" &>out4_naive_counting && git diff out4_naive_counting
+
+./driver_brute_force <"in"  &> out_brute_force && git diff out_brute_force
+./driver_brute_force <"in2" &>out2_brute_force && git diff out2_brute_force
+./driver_brute_force <"in3" &>out3_brute_force && git diff out3_brute_force
+./driver_brute_force <"in4" &>out4_brute_force && git diff out4_brute_force
