e3a48ee203321e50ec19144a07ec983d31c3f650
Karel Ha <mathemage@gmail.com>
2021-02-05 00:12:13 +0100

    Quora Haqathon - Upvotes
    Include official testcase #4/29 (WA)
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- quora/Quora-Haqathon/upvotes/gd_out.sh
+++ quora/Quora-Haqathon/upvotes/gd_out.sh
@@ -15,13 +15,15 @@ clear
 # ./driver_brute_force <"in3" &>out3_brute_force && git diff out3_brute_force
 # ./driver_brute_force <"in4" &>out4_brute_force && git diff out4_brute_force
 
-# ./driver_brute_force_v2 <"in"  &> out_brute_force && git diff out_brute_force
-# ./driver_brute_force_v2 <"in2" &>out2_brute_force && git diff out2_brute_force
-# ./driver_brute_force_v2 <"in3" &>out3_brute_force && git diff out3_brute_force
-# ./driver_brute_force_v2 <"in4" &>out4_brute_force && git diff out4_brute_force
+./driver_brute_force_v2 <"in"  &> out_brute_force && git diff out_brute_force
+./driver_brute_force_v2 <"in2" &>out2_brute_force && git diff out2_brute_force
+./driver_brute_force_v2 <"in3" &>out3_brute_force && git diff out3_brute_force
+./driver_brute_force_v2 <"in4" &>out4_brute_force && git diff out4_brute_force
+./driver_brute_force_v2 <"./in_04_29" &>out_04_29_brute_force && git diff out_04_29_brute_force
 
-./driver <"in"  &> out_dp && git diff out_dp
-./driver <"in2" &>out2_dp && git diff out2_dp
-./driver <"in3" &>out3_dp && git diff out3_dp
-./driver <"in4" &>out4_dp && git diff out4_dp
+./driver <"in"         &> out_dp && git diff out_dp
+./driver <"in2"        &>out2_dp && git diff out2_dp
+./driver <"in3"        &>out3_dp && git diff out3_dp
+./driver <"in4"        &>out4_dp && git diff out4_dp
+./driver <"./in_04_29" &>out_04_29_dp && git diff out_04_29_dp
 cat out*dp
