1b7596da5dcb43a748fb36026c75c822840a7069
Karel Ha <mathemage@gmail.com>
2021-02-05 20:38:38 +0100

    Quora CodeSprint - Quora Nearby
    Add testcase #6/9 to `gd_out.sh`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- quora/CodeSprint-Quora/quora-nearby/gd_out.sh
+++ quora/CodeSprint-Quora/quora-nearby/gd_out.sh
@@ -2,5 +2,6 @@ clear
 
 ./driver_brute_force <"in"  &> out_brute_force && git diff out_brute_force
 ./driver_brute_force <"./in03_09" &>out03_09_brute_force && vimdiff out03_09_brute_force out03_09
+./driver_brute_force <"./in06_09" &>out06_09_brute_force && vimdiff out06_09_brute_force out06_09
 
 # cat out*dp
