9c548c9bef78be5d1fb971bb036f1be7c7a372ce
Karel Ha <mathemage@gmail.com>
2021-02-08 00:50:51 +0100

    Codeforces #16B, Div 2
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/16/B/B.cpp
@@ -0,0 +1,89 @@
+ 
+/* ========================================
+   * File Name : B.cpp
+   * Creation Date : 08-02-2021
+   * Last Modified : Po 8. února 2021, 00:44:50
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/problemset/problem/16/B
+   * Points/Time :
+   * - 10m 40s
+   * - 13m 00s
+   *
+   * Total/ETA : 8m
+   * Status :
+   * - WA (#5)
+   * - AC !!
+   *
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define FO(I,A,B) for(int I = (A); I <= (B); ++I)
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define RFOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define SETCONTAINS(S,E) ((S).find(E) != (S).end())
+#define MSG(a) cerr << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(m);
+  vector<int> b(m);
+  vector<pair<int, int>> sizes_counts(m);
+  REP(i,m) {
+    cin >> a[i] >> b[i];
+    sizes_counts.PB(MP(b[i], a[i]));
+  }
+
+  long long result = 0LL;
+  int available_count;
+  sort(REVALL(sizes_counts));
+  for (auto & size_count: sizes_counts) {
+    if (n>0) {
+      available_count = min(size_count.S, n);
+      result += size_count.F * available_count;
+      n -= available_count;
+    } else {
+      break;
+    }
+  }
+  cout << result << endl;
+}
+
+int main() {
+  int cases = 1;
+  while (cases--) {
+    solve();
+  }
+  return 0;
+}
