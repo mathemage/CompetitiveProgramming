b0ff7017588a09a34efee2c94a56cc0ced9dfc3e
Karel Ha <mathemage@gmail.com>
2021-02-11 00:57:24 +0100

    Codeforces #487A, Div 2
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/989/A/A.bak01.cpp
@@ -0,0 +1,85 @@
+ 
+/* ========================================
+   * File Name : A.cpp
+   * Creation Date : 11-02-2021
+   * Last Modified : Čt 11. února 2021, 00:44:27
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/problemset/problem/989/A
+   * Points/Time :
+   * - 7m 40s
+   *
+   * Total/ETA : 10m
+   * Status :
+   * - WA #17
+   *
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define FO(I,A,B) for(int I = (A); I <= (B); ++I)
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define RFOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define SETCONTAINS(S,E) ((S).find(E) != (S).end())
+
+#define MSG(a) cerr << "> " << #a << ": " << (a) << endl;
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+string s;
+
+void solve() {
+  cin >> s;
+  int n = s.size();
+
+  bool result = false;
+  FO(i,1,n-2) {
+    if (s[i-1] + s[i] + s[i+1] == 'A' + 'B' + 'C') {
+      result = true;
+      break;
+    }
+  }
+
+  cout << (result?"YES":"NO") << endl;
+}
+
+int main() {
+  int cases = 1;
+  while (cases--) {
+    solve();
+  }
+  return 0;
+}
