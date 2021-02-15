d9ee7026d34171129ddd3927e24932709e043c57
Karel Ha <mathemage@gmail.com>
2021-02-16 00:45:15 +0100

    Codeforces #114A, Div 2
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/168/A/A.cpp
@@ -0,0 +1,78 @@
+ 
+/* ========================================
+   * File Name : A.cpp
+   * Creation Date : 16-02-2021
+   * Last Modified : Út 16. února 2021, 00:38:21
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/problemset/problem/168/A
+   * Points/Time :
+   * - 5m 50s
+   * - 7m 30s
+   *
+   * Total/ETA : 5m 30s
+   * Status :
+   * - WA #4
+   * - AC
+   *
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+
+#define MSG(a) cerr << "> " << #a << ": " << (a) << endl;
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+int n,x,y;
+
+void solve() {
+  cin >> n >> x >> y;
+  long long result = (n*y+99)/100 - x;
+  cout << max(0LL,result) << endl;
+}
+
+int main() {
+  int cases = 1;
+  while (cases--) {
+    solve();
+  }
+  return 0;
+}
