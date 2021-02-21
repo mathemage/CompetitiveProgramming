b20a3b4a6acc9dd61978afcb8de2e0e9192cf2b9
Karel Ha <mathemage@gmail.com>
2021-02-21 23:55:05 +0100

    TopCoder Dynamic-Programming-Practice-DIV-1-200: NumberofFiboCalls
    coding kata 1
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/Dynamic-Programming-Practice-DIV-1/200/NumberofFiboCalls_coding_kata_1.cc
@@ -0,0 +1,72 @@
+/* ========================================
+ * Points/Time : 169.81 (11m 40s)
+ * Total/ETA : 200 (5m)
+ * Status : AC
+ ==========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+
+#define MSG(a) cerr << "> " << #a << ": " << (a) << endl;
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+class NumberofFiboCalls {
+public:
+  vector <int> fiboCallsMade(int n) {
+    vector<int> zeros(n+1,0);
+    zeros[0]=1;
+//     zeros[1]=0;
+
+    vector<int> ones(n+1,0);
+//     ones[1]=0;
+    ones[1]=1;
+
+    FOR(i,2,n) {
+      zeros[i]=zeros[i-1]+zeros[i-2];
+      ones[i]=ones[i-1]+ones[i-2];
+    }
+
+    print_vector(zeros, "zeros");
+    print_vector(ones, "ones");
+
+    vector <int> result = {zeros[n], ones[n]};
+    return result;
+  }
+};
