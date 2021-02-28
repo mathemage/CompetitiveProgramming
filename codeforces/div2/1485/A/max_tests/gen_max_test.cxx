59e655de3e499ab178d1197cdc7186e20af824c5
Karel Ha <mathemage@gmail.com>
2021-02-28 22:06:33 +0100

    Codeforces #701A, Div 2
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/1485/A/max_tests/gen_max_test.cxx
@@ -0,0 +1,51 @@
+ 
+/* ========================================
+
+   ID: mathema6
+   TASK: gen_max_test
+   LANG: C++11
+
+   * File Name : gen_max_test.cxx
+   * Creation Date : 07-01-2021
+   * Last Modified : Ne 28. února 2021, 16:35:10
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL :
+   * Points :
+   * Total :
+   * Status :
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define RFOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define MSG(a) cerr << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+
+void solve() {
+  long long result = 0LL;
+  cout << result << endl;
+}
+
+int main() {
+  // t
+  int cases = 100;
+  cout << cases << endl;
+
+  while (cases--) {
+    // n, m...
+    int a = 1e9;
+    int b = 1;
+    cout << a << " " << b << endl;
+  }
+
+  return 0;
+}
