b03417e7e1e6686c68123f630b8d0b6ecd1483cd
Karel Ha <mathemage@gmail.com>
2021-03-04 19:04:56 +0100

    Codeforces #703A, Div 2
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/1486/A/A.cpp
@@ -0,0 +1,89 @@
+ 
+/* ========================================
+   * File Name : A.cpp
+   * Creation Date : 04-03-2021
+   * Last Modified : Čt 4. března 2021, 18:58:22
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/contest/1486/problem/A
+   * Points/Time : 11m 50s
+   * Total/ETA : 8m
+   * Status : AC
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#define MSG(a) cerr << "> " << #a << ": " << (a) << endl;
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  bool result = true;
+  int n, hi;
+  cin >> n;
+  long long carry=0;
+  REP(i,n) {
+//     MSG(carry)
+    cin >> hi;
+    carry+=hi-i;
+    result &= carry>=0;
+//     MSG(hi) MSG(carry) cerr << endl;
+  }
+  cout << (result?"YES":"NO") << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int cases = 1;
+  cin >> cases;
+  while (cases--) {
+    solve();
+  }
+  return 0;
+}
