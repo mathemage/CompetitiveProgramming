77a4ecf00d1ad969cbe871be345b12b434afccf7
Karel Ha <mathemage@gmail.com>
2021-03-05 16:52:26 +0100

    TopCoder Rookie-SRM-1-Div-1-250: ColorWheel
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/Rookie-SRM-1-DIV-1/250/ColorWheel_coding_kata_2.cc
@@ -0,0 +1,76 @@
+/* ========================================
+ * Points/Time :
+ * 247.44
+ *
+ * Total/ETA : 250
+ * Status :
+ * AC
+ *
+ ==========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#define MSG(a) cerr << "> " << #a << ": " << (a) << endl;
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+// direction vectors - 4 directions
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+// direction vectors - 8 directions
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+class ColorWheel {
+public:
+  string describePair(string color1, string color2) {
+    string col="ROYGBP";
+    int i1=col.find(color1[0]);
+    int i2=col.find(color2[0]);
+
+    string result="None";
+    if (i1==i2) {
+      result="Same";
+    } else if ( (i1-i2+6) % 6 == 3) {
+      result="Complementary";
+    } else if ( abs(i1-i2)==1 || abs(i1-i2)==5 ) {
+      result="Adjacent";
+    }
+    return result;
+  }
+};
