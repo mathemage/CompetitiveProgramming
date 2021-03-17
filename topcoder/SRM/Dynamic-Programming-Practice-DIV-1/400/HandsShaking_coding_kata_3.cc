b278d466a76337ea2a348349ac2881b98929f3ca
Karel Ha <mathemage@gmail.com>
2021-03-17 16:25:30 +0100

    TopCoder DP-Div-1-400: HandsShaking
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/Dynamic-Programming-Practice-DIV-1/400/HandsShaking_coding_kata_3.cc
@@ -0,0 +1,98 @@
+/* ========================================
+ * Points/Time : 396.63
+ * Total/ETA : 400
+ * Status : AC
+ ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC(v) print_vector((v), (#v));
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+class HandsShaking {
+public:
+  long long countPerfect(int n) {
+    LINESEP2;
+
+    n/=2;
+    vector<long long> dp(n+5);
+    dp[0]=1;
+    REP(k,n+1) {
+      REP(i,k) {
+        dp[k] += dp[i] * dp[k-1-i];
+      }
+    }
+    return dp[n];
+  }
+};
---
+++ topcoder/SRM/Dynamic-Programming-Practice-DIV-1/400/t.cc
@@ -0,0 +1,78 @@
+/* ========================================
+ * Points/Time : 261.11
+ * Total/ETA : 400
+ * Status : AC
+ ==========================================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#define MSG(a) cerr << "> " << #a << ": " << (a) << endl;
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+template<typename T>
+void print_vector_pairs(const vector<pair<T, T>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+class HandsShaking {
+public:
+  long long countPerfect(int n) {
+    vector<long long> ways(n+5);
+    ways[0]=1;
+    for (int k = 0; k < SZ(ways); k += 2) {
+      for (int i = 0; i < k; i += 2) {
+        ways[k] += ways[i] * ways[k-2-i];
+      }
+    }
+    print_vector(ways, "ways");
+    return ways[n];
+  }
+};
