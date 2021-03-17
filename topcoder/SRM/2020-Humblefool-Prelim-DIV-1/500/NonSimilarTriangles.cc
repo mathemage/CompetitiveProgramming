36eff30ac730549c0e945751a0d3e2601eccc032
Karel Ha <mathemage@gmail.com>
2021-03-17 01:18:17 +0100

    TopCoder Humblefool'20 Prelims-500: NonSimilarTriangles
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/2020-Humblefool-Prelim-DIV-1/500/NonSimilarTriangles.cc
@@ -0,0 +1,106 @@
+/* ========================================
+ * Points/Time : 423.60 (12m30s)
+ * Total/ETA : 500
+ * Status : AC
+ ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC(v) print_vector((v), (#v));
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+class NonSimilarTriangles {
+public:
+  int count(vector <int> L) {
+    LINESEP2;
+
+    sort(ALL(L));
+    set<tuple<int, int, int> > result;
+
+    int n=SZ(L);
+    FOR(i,0,n-1) FOR(j,i+1,n-1) FOR(k,j+1,n-1) {
+      MSG(L[i]) MSG(L[j]) MSG(L[k])
+      if (L[i]+L[j]>L[k]) {
+        int d=__gcd(L[i],__gcd(L[j],L[k]));
+        int a=L[i]/d;
+        int b=L[j]/d;
+        int c=L[k]/d;
+        result.insert(MTP(a,b,c));
+        MSG(d)
+        MSG(a) MSG(b) MSG(c)
+      }
+    }
+    return SZ(result);
+  }
+};
