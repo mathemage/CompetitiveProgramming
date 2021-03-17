952bc22ec3c278f9169e84faec79ae1729cfa158
Karel Ha <mathemage@gmail.com>
2021-03-17 18:36:06 +0100

    Compete in TopCoder Humblefool'21 Prelims
    successful challenges: 2/3 (50+50-25=75 pts) ^_^
    
    rank in the room: 7/9 (~10 competing, 12 present)
    
    rank in the division: 109/714 (~109/139 present??)
    -> 85th PERCENTILE (22nd PERCENTILE??)
    <- 3 blue coders below me !
    <- 1 yellow coder below me !!
    
    Notes:
    - could have been 102/714 (~102/139??) of the division w/o the failed challenge :-/
    -> would have been 85th PERCENTILE (27th PERCENTILE??)
    
    Blunders:
    - wrong original approach in 300
      - tried to iterate over divisors a*b==N
      - fails on the last test
      -> successful challenge of utkar5h_7 (same approach!)
    - infinite while-loop somewhere in 450
      - had to restart my laptop twice -> lost shitload of time :-(
      -> don't use while-loop, always prefer for-loop
         - or add a timeout variable
      -> COMPETE ON DESKTOP from now on, if possible
    - tried to challenge a red coder w/o understanding really his code
      - only saw ++ in a while -> tried :-/
      - ALWAYS UNDERSTAND THE CODE
      - if you risk, don't risk against yellow and higher
    - the challenge of the red coder: same code as the other successful
    challenge
      - mystery!
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/2021-Humbefool-Cup/450/MoveAllMarbles.cc
@@ -0,0 +1,141 @@
+/* ========================================
+ * Points/Time :
+ * Total/ETA :
+ * Status :
+ ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC(v) print_vector((v), (#v));
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+class MoveAllMarbles {
+public:
+  vector <int> move(int R, int C, int N) {
+    LINESEP2;
+
+    vector <int> result;
+
+    // oldr, oldc, newr, newc
+
+    // load
+    int nn=0;
+    FORD(er,R-1,0) FORD(ec,C-1,0) {
+      if ((er==0 && ec==0) || (er==R-1 && ec==C-1)) {
+        continue;
+      }
+
+      int sr=0, sc=0;
+      while (sc<ec) {
+        MSG(sr) MSG(sc);
+        result.PB(sr), result.PB(sc);
+        result.PB(sr), result.PB(++sc);
+        MSG(sr) MSG(sc);
+      }
+      while (sr<er) {
+        MSG(sr) MSG(sc);
+        result.PB(sr), result.PB(sc);
+        result.PB(++sr), result.PB(sc);
+        MSG(sr) MSG(sc);
+      }
+      if (++nn>=N) {
+        break;
+      }
+    }
+    MSG("loaded")
+
+    // dispatch
+    nn=0;
+    FORD(sr,R-1,0) FORD(sc,C-1,0) {
+      if ((sr==0 && sc==0) || (sr==R-1 && sc==C-1)) {
+        continue;
+      }
+
+      while (sc<C-1) {
+        result.PB(sr), result.PB(sc);
+        result.PB(sr), result.PB(++sc);
+      }
+      while (sr<R-1) {
+        result.PB(sr), result.PB(sc);
+        result.PB(++sr), result.PB(sc);
+      }
+      if (++nn>=N) {
+        break;
+      }
+    }
+    MSG("dispatched")
+
+    return result;
+  }
+};
