7be892919001e6ce88ff90d5c218f6cd40ceedf1
Karel Ha <mathemage@gmail.com>
2021-03-20 14:41:07 +0100

    Compete in AtCoder ABC 196
    place in the division: #3321/#8503-8650 (page 166-167/526)
    -> 61st PERCENTILE (COVID-19)
    rank in the division: #3318/8630
    -> 62nd PERCENTILE (COVID-19) :-/
    rating: +134 -> 247 (provisional) [11 KYU -> 9 KYU]
    performance as 810
    
    Notes:
    - bad luck with D (no idea how to solve)
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/196/C/C.cpp
@@ -0,0 +1,116 @@
+/* ========================================
+   * File Name : C.cpp
+   * Creation Date : 20-03-2021
+   * Last Modified : Sat 20 Mar 2021 01:37:08 PM CET
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://atcoder.jp/contests/abc196/tasks/abc196_c
+   * Points/Time :
+   * Total/ETA : 300
+   * Status : AC !!!
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC(v) print_vector((v), (#v));
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  long long n;
+  cin >> n;
+
+//   for (int half = 1; 2*half <= 12; half += 1) {
+//   }
+
+  long long result = 0LL;
+  FOR(k,1LL,1e6-1) {
+    long long kk= stoll(to_string(k)+to_string(k));
+    result += kk<=n;
+  }
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int cases = 1;
+  while (cases--) {
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
