8100e9873dde0f3f6449a63de572b2f5bfb5283f
Karel Ha <mathemage@gmail.com>
2021-03-23 22:18:39 +0100

    Train USACO 1.1: ride
    Resolve again with cxx template
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ usaco/train.usaco.org/1.1/ride/ride.cxx
@@ -0,0 +1,154 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: ride
+   LANG: C++14
+
+   * File Name : ride.cxx
+   * Creation Date : 23-03-2021
+   * Last Modified : Tue 23 Mar 2021 10:13:48 PM CET
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://train.usaco.org/usacoprob2?a=R0ESMaCy7HI&S=ride
+   * Points/Time :
+   *  2m
+   * +7m50s = 9m50s
+   * +4m
+   * +3m    =16m50s
+   *
+   * Total/ETA :
+   * Status : 
+   * WA #1
+   * AC
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "ride"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC(v) print_vector((v), (#v));
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define MSG_VEC_STR(v) print_vector_string((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define MSG_VEC_STR(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+template<typename T>
+void print_vector(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << x << "  ";
+  cerr << endl;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+void print_vector_string(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": " << endl;
+  for (const auto & s: vec) cerr << s << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  string comet, group;
+  cin >> comet >> group;
+
+  vector<long long> n(2, 1);
+  int i=0;
+  for (auto & s: {comet, group}) {
+    for (auto & c: s) {
+      n[i]*=c-'A'+1LL;
+      MSG(c); MSG(c-'A'+1LL); LINESEP1;
+    }
+    MSG(i); MSG(n[i]);
+    i++;
+  }
+  bool result = n[0]%47==n[1]%47;
+  cout << (result?"GO":"STAY") << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+  setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  while (cases--) {
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
