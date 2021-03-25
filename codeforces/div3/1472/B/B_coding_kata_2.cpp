f99d386fad1fa9194cf361a807284901dd44fa16
Karel Ha <mathemage@gmail.com>
2021-03-25 01:10:52 +0100

    Codeforces #693B, Div 3
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div3/1472/B/B_coding_kata_2.cpp
@@ -0,0 +1,159 @@
+/* ========================================
+   * File Name : B_coding_kata_2.cpp
+   * Creation Date : 25-03-2021
+   * Last Modified : Thu 25 Mar 2021 01:02:53 AM CET
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : /home/mathemage/src/github/mathemage/CompetitiveProgramming/codeforces/div3/1472/B
+   * Points/Time : 7m
+   * Total/ETA :
+   * Status : AC
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define MSG_VEC_STR(v) print_vector_string((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define MSG_VEC_STR(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+void print_vector_string(const vector<T> & vec, const string & name) {
+  cerr << "> " << name << ": " << endl;
+  for (const auto & s: vec) cerr << s << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  int n;
+  cin >> n;
+
+  map<int,int> cnt;
+  int ai;
+  while (n--) {
+    cin >> ai;
+    cnt[ai]++;
+  }
+
+  bool result = false;
+  int sum=cnt[1]+2*cnt[2];
+  MSG(sum);
+  if (sum%2) {
+    result=false;
+  } else {
+    int need=sum/2;
+    MSG(cnt[1]); MSG(cnt[2]);
+    MSG(need);
+    FOR(k,0,cnt[2]) {
+      if (2*k>need) {
+        break;
+      }
+
+      if (cnt[1] >= need-2*k) {
+        MSG(k);
+        result=true;
+        break;
+      }
+    }
+  }
+
+  cout << (result?"YES":"NO") << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int cases = 1;
+  cin >> cases;
+  while (cases--) {
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
