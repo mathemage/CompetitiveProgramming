b21662a09535e77ce06893a8de2fd503cfdf9fb6
Karel Ha <mathemage@gmail.com>
2021-03-27 15:25:28 +0100

    Compete in AtCoder ABC 197
    place in the division: #3352/#7786 (page 168/503)
    -> 57th PERCENTILE :-/ :-(  (COVID-19)
    rank in the division: #3351/7885
    -> 58th PERCENTILE :-/ (COVID-19)
    rating: +89 -> 336 (provisional) [9 KYU]
    performance as 728 :-/
    
    Notes:
    - blind to easy brute force (bitwise exhaustive search) in C
      - even after bad lesson from ABC196D
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/197/D/D.cpp
@@ -0,0 +1,146 @@
+/* ========================================
+   * File Name : D.cpp
+   * Creation Date : 27-03-2021
+   * Last Modified : Sat 27 Mar 2021 07:43:53 PM CET
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://atcoder.jp/contests/abc197/tasks/abc197_d
+   * Points/Time :
+   * Total/ETA : 400pts
+   * Status :
+   * unsubmitted during contest
+   * AC (1-2m after contest)
+   *
+   ==========================================*/
+
+#include <bits/stdc++.h>
+#include <cmath>
+#include <iterator>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) cerr << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+// const double PI = 3.14159265;
+const double PI = M_PI;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  long long N;
+  cin >> N;
+  double x0,y0,xx,yy;
+  cin >> x0 >> y0 >> xx >> yy;
+
+  double sx=(x0+xx)/2, sy=(y0+yy)/2;
+  double angle=2*PI/N;
+  double s = sin(angle);
+  double c = cos(angle);
+
+  x0-=sx, y0-=sy; 
+  double xr=c*x0-s*y0;
+  double yr=s*x0+c*y0;
+  xr+=sx, yr+=sy; 
+
+  cout << xr << " " << yr << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int cases = 1;
+  while (cases--) {
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
