7d15a09874b417d120c10bc060de1c505a9a80e8
Karel Ha <mathemage@gmail.com>
2021-03-29 00:44:18 +0200

    Train USACO 1.1: gift1
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ usaco/train.usaco.org/1.1/gift1/gift1.cxx
@@ -0,0 +1,180 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: gift1
+   LANG: C++14
+
+   * File Name : gift1.cxx
+   * Creation Date : 29-03-2021
+   * Last Modified : Mon 29 Mar 2021 12:33:20 AM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://train.usaco.org/usacoprob2?a=XCF1lvlCutc&S=gift1
+   * Points/Time :
+   * ~10m
+   * +17m50s = 27m50s
+   *  (bug with re-resetting accounts instead of just decrementing -=)
+   * 
+   * Total/ETA :
+   * Status : AC
+   ==========================================*/
+
+#define PROBLEMNAME "gift1"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) cerr << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  int NP;
+  cin >> NP;
+
+  string donor;
+  map<string,int> account;
+  vector<string> names;
+  REP(i,NP) {
+    cin >> donor;
+//     account[donor]=0;
+    names.PB(donor);
+  }
+  MSG(names);
+
+  string recipient;
+  int amount, NGi;
+  REP(j,NP) {
+    cin >> donor;
+    cin >> amount >> NGi;
+
+    account[donor]-=amount;
+    REP(i,NGi) {
+      cin >> recipient;
+      account[recipient]+=amount/NGi;
+    }
+    if (NGi) {
+      account[donor]+=amount%NGi;
+    }
+//     for (auto & name: names) { cerr << account[name] << "\t"; }
+//     cerr << endl;
+  }
+
+  for (auto & name: names) {
+    cout << name << " " << account[name] << endl;
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+  setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  while (cases--) {
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
