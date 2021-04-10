16705e6fe91ab8bd0bc0be58317ed79b979b97f5
Karel Ha <mathemage@gmail.com>
2021-04-10 05:32:37 +0200

    Compete in Google CodeJam '21 Round 1A
    points: 0/100 pts
    rank: -/~8200 (~10000 contestants submitted something)
    -> last PERCENTILE :-(
    
    Round overview:
    "More than 8200 people scored some points out of the more than 10000
    that submitted a solution." :-/
    
    "When the hidden results were revealed, the contestant in 1500th place
    had 54 points, which is the unofficial cutoff for advancing."
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gcodejam/2021/round-1A/A/A.cpp
@@ -0,0 +1,200 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: 
+   LANG: C++14
+
+   * File Name : A.cpp
+   * Creation Date : 10-04-2021
+   * Last Modified : Sat 10 Apr 2021 05:35:07 AM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/codejam/round/000000000043585d/00000000007549e5
+   * Points/Time :
+   * ~2h20m
+   * 2h30m -> practice mode already :-( :-( :-(
+   *
+   * Total/ETA : 15m
+   * Status :
+   * S WA - :-(
+   * S AC WA :-/
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "TASK_PLACEHOLDER_FOR_VIM"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) os << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {-1, 0, 1,  0};
+const vector<int> DY4 = { 0, 1, 0, -1};
+const vector<pair<int,int>> DXY4 = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+void solve() {
+  int N;
+  cin >> N;
+  vector<ul> X(N);
+
+  REP(i,N) {
+    cin >> X[i];
+  }
+  MSG(X);
+
+  ul result = 0LL;
+  FOR(i,1,N-1) {
+    MSG(X[i-1]); MSG(X[i]);
+
+    if (X[i-1]==X[i]) {
+      X[i]*=10;
+      result++;
+    } else if (X[i-1]>X[i]) {
+      ul newNum=X[i];
+      string s=to_string(X[i-1]);
+      string s999=to_string(X[i]);
+      while (s.size() > to_string(newNum).size()) {
+        newNum*=10;
+        result++;
+
+        s999+='9';
+      }
+      MSG(newNum) MSG(s999); LINESEP1;
+
+      if (newNum<=X[i-1]) {
+        string ss=to_string(X[i]);
+        MSG(s.substr(0,SZ(ss))); MSG(ss);
+
+        if (s.substr(0,SZ(ss))==ss && s!=s999) {
+          newNum=X[i-1]+1;
+        } else {
+          newNum*=10;
+          result++;
+        }
+      }
+
+      X[i]=newNum;
+    }
+  }
+  MSG(X);
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
