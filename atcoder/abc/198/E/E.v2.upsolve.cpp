092b0fd26b4946574edda2e71e7c233ca6d0cb63
Karel Ha <mathemage@gmail.com>
2021-04-13 00:42:24 +0200

    Upsolve AtCoder ABC 198E - Unique Color
    - Yes, flash upsolve -> let's go to bed!
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/198/E/E.v2.upsolve.cpp
@@ -0,0 +1,201 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: 
+   LANG: C++14
+
+   * File Name : E.cpp
+   * Creation Date : 11-04-2021
+   * Last Modified : Tue 13 Apr 2021 12:41:49 AM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://atcoder.jp/contests/abc198/tasks/abc198_e
+   * Points/Time : 23m30s
+   * Total/ETA : 500
+   * Status : AC !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! YES, flash!!
+   ==========================================*/
+
+#define PROBLEMNAME "TASK_PLACEHOLDER_FOR_VIM"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) os << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const multiset<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {-1, 0, 1,  0};
+const vector<int> DY4 = { 0, 1, 0, -1};
+const vector<pair<int,int>> DXY4 = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+set<int> result;
+vector<int> C;
+map<int,vector<int>> neigh;
+multiset<int> usedC;
+vector<bool> vis;
+
+void dfs(int root) {
+  LINESEP1; MSG(root);
+
+  vis[root]=true;
+  if (!CONTAINS(usedC, C[root])) {
+    result.insert(root);
+  }
+  auto ins = usedC.insert(C[root]);
+
+  MSG(neigh[root]);
+  for (auto & child: neigh[root]) {
+    if (!vis[child]) {
+      MSG(child); MSG(usedC);
+      dfs(child);
+    }
+  }
+
+//   usedC.erase(C[root]);
+  usedC.erase(ins);
+}
+
+void solve() {
+  ll N;
+  cin >> N;
+
+  C.resize(N+1);
+  FOR(i,1,N) { cin >> C[i]; }
+  MSG(C);
+
+  int Ai,Bi;
+  REP(i,N) {
+    cin >> Ai >> Bi;
+    neigh[Ai].PB(Bi);
+    neigh[Bi].PB(Ai);
+  }
+
+  usedC.clear();
+  vis.resize(N+1);
+  dfs(1);
+
+//   sort(ALL(result));
+  for (auto & it: result) { cout << it << endl; }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int cases = 1;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
