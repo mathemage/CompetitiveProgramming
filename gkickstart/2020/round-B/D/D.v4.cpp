2684bd3ec62c0f73fbc34d626bf13f2e24f0a5c8
Karel Ha <mathemage@gmail.com>
2021-04-15 16:18:12 +0200

    GKickStart '20B, D. Wandering Robot
    - not solved (yet)
    S AC TLE
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gkickstart/2020/round-B/D/D.v4.cpp
@@ -0,0 +1,232 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: 
+   LANG: C++14
+
+   * File Name : D.cpp
+   * Creation Date : 15-04-2021
+   * Last Modified : Thu 15 Apr 2021 03:51:31 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ffc8/00000000002d8565
+   * Points/Time :
+   *   5m
+   * +27m = 32m
+   * + 7m = 39m
+   * +11m = 50m
+   * + 3m = 53m
+   *
+   * Total/ETA : 40m
+   * Status :
+   * S AC MLE
+   * S AC TLE
+   * S AC TLE
+   * S WA -
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "TASK_PLACEHOLDER_FOR_VIM"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) os << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T>
+ostream& operator<<(ostream& os, const multiset<T>& vec) {
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) {
+  for (auto & x: vec) is >> x;
+  return is;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {-1, 0, 1,  0};
+const vector<int> DY4 = { 0, 1, 0, -1};
+const vector<pair<int,int>> DXY4 = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  int W, H, L, U, R, D;
+  cin >> W >> H >> L >> U >> R >> D;
+
+  MINUPDATE(W, R+1);
+  MINUPDATE(H, D+1);
+
+  int nRows=1+H, nCols=1+W;
+  vector<double> dp(nCols);
+  dp[W]=1.0;
+
+  double nNeigh,sum;
+  int rowTimer=0;
+//   const int rowTimeout=1000;
+  const int rowTimeout=100;
+  FORD(r,H,1) {
+    FORD(c,W,1) {
+      if (U<=r && r<=D && L<=c && c<=R) {
+        dp[c] = 0.0;
+        continue;
+      }
+
+      sum = nNeigh=0.0;
+      if (c+1<nCols) {
+        sum+=dp[c+1];
+        nNeigh++;
+      }
+      if (r+1<nRows) {
+        sum+=dp[c];
+        nNeigh++;
+      }
+
+      if (nNeigh) { dp[c]=sum/nNeigh; }
+    }
+
+    MSG(r); MSG_VEC_VEC(dp);
+
+    // optimize rows
+    if (++rowTimer>rowTimeout) {
+      if (r>U) {
+        r=U;
+      } else {
+        MINUPDATE(r, rowTimeout);
+      }
+      rowTimer=0;
+    }
+
+    LINESEP1;
+  }
+  MSG_VEC_VEC(dp);
+
+  cout << dp[1] << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
