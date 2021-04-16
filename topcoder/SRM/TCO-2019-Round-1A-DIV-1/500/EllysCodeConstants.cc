a31b5841c18f7e0f1a952073d8dcc0eab2433265
Karel Ha <mathemage@gmail.com>
2021-04-16 16:27:30 +0200

    TopCoder TCO-2019-Round-1A-DIV-1-500: EllysCodeConstants
    337.90/500 (WA - AC)
    
    Challenged "marron_kuri" (target red coder)
    - with the same WA ("UL" -> should be "", not "0xUL")
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/TCO-2019-Round-1A-DIV-1/500/EllysCodeConstants.cc
@@ -0,0 +1,185 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: 
+   LANG: C++14
+
+   * File Name :
+   * Creation Date :
+   * Last Modified :
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL :
+   * Points/Time :
+   *   4m
+   * +10m = 14m (446.20)
+   * + 4m = 18m (337.90)
+   *
+   * Total/ETA : 500
+   * Status :
+   * WA :-/
+   * AC
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "TASK_PLACEHOLDER_FOR_VIM"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n"; (print_vector_vector(cerr, (v))) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+
+  #define MSG_VEC(v) cerr << "> " << (#v) << ": "; (print_vector(cerr, (v))) << endl;
+  #define MSG_VEC_STR(v) cerr << "> " << (#v) << ": "; (print_vector_string(cerr, (v))) << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+
+  #define MSG_VEC(v)
+  #define MSG_VEC_STR(v)
+#endif
+
+inline ostream& print_vector_string(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) cerr << s << endl;
+  return os;
+}
+
+template<typename T> 
+inline ostream& print_vector(ostream& os, const vector<T> & vec) {
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T>
+inline ostream& print_vector_vector(ostream& os, const vector<vector<T>> & vec) {
+  for (const auto & line: vec) {
+    for (const auto & it: line) {
+      os << it << " ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+// template<typename T> 
+// ostream& operator<<(ostream& os, const set<T>& vec) { 
+//   os << "{ | ";
+//   for (const auto & x: vec) os << x << "| ";
+//   os << "}";
+//   return os; 
+// } 
+// 
+// template<typename T>
+// ostream& operator<<(ostream& os, const multiset<T>& vec) {
+//   os << "{ | ";
+//   for (const auto & x: vec) os << x << "| ";
+//   os << "}";
+//   return os;
+// }
+//
+// template<typename T1, typename T2>
+// void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+//   cerr << "> " << name << ": ";
+//   for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+//   cerr << endl;
+// }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) {
+  return min(l,u)<=x && x<max(l,u);
+}
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = { 0, 0, -1, 1};
+const vector<int> DY4 = {-1, 1,  0, 0};
+const vector<pair<int,int>> DXY4 = { {0,-1}, {0,1}, {-1,0}, {1,0} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+class EllysCodeConstants {
+public:
+  string getLiteral(string candidate) {
+    LINESEP2;
+
+    string result="0x";
+    int n=SZ(candidate);
+    for (int i = 0; i < candidate.size(); i += 1) {
+      char ch=candidate[i];
+      if ('A'<=ch && ch<='F') { result+=ch; }
+      else if (ch=='O') { result+='0'; }
+      else if (ch=='I') { result+='1'; }
+      else if (ch=='Z') { result+='2'; }
+      else if (ch=='S') { result+='5'; }
+      else if (ch=='T') { result+='7'; }
+      else {
+        if (i>=1 && i==n-1 && (ch=='L' || ch=='U')) { result+=ch; }
+        else if (i>=1 && i==n-2
+            && (candidate.substr(i,2)=="LL" || candidate.substr(i,2)=="UL" || candidate.substr(i,2)=="LU") ) { result+=candidate.substr(i,2); }
+        else if (i>=1 && i==n-3
+            && (candidate.substr(i,3)=="ULL" || candidate.substr(i,3)=="LLU") ) { result+=candidate.substr(i,3); }
+        else {
+          return "";
+        }
+        break;
+      }
+    }
+    return result;
+  }
+};
+
+/*
+Remember:
+
+1) (RE-)COMPILE LAST VERSION BEFORE SUBMITTING!!!!!
+2) CHECK ADDITIONALLY IN THE ROOM SUMMARY (whether CORRECT source code was submitted)!!!!
+  - Or `vimdiff solutionName*` should produce no diffs!
+3) Never submit brute-force/inferior solution when it might be too slow!!
+  - Or always re-compile again!
+*/
