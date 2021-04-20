60f7030bd9120312f01f500a495ec40dc57f430b
Karel Ha <mathemage@gmail.com>
2021-04-20 23:09:12 +0200

    Update syntax-link.sh
    Use $CP_DIR in symlinks
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- vim/syntax/syntax-link.sh
+++ vim/syntax/syntax-link.sh
@@ -1,7 +1,11 @@
-ln -s ~/prog/CompetitiveProgramming/vim/syntax/c++-abbreviations.vim c++-abbreviations.vim
-ln -s ~/prog/CompetitiveProgramming/vim/syntax/cc.vim cc.vim
-ln -s ~/prog/CompetitiveProgramming/vim/syntax/cpp.template cpp.template
-ln -s ~/prog/CompetitiveProgramming/vim/syntax/cpp-trung.vim cpp-trung.vim
-ln -s ~/prog/CompetitiveProgramming/vim/syntax/cxx.template cxx.template
-ln -s ~/prog/CompetitiveProgramming/vim/syntax/cu.vim cu.vim
-ln -s /home/mathemage/prog/CompetitiveProgramming/vim/.vimrc ~/.vimrc
+CP_DIR=/home/mathemage/src/github/mathemage/CompetitiveProgramming/
+
+ln -s $CP_DIR/vim/syntax/c++-abbreviations.vim c++-abbreviations.vim
+ln -s $CP_DIR/vim/syntax/cc.vim cc.vim
+ln -s $CP_DIR/vim/syntax/cpp.template cpp.template
+ln -s $CP_DIR/vim/syntax/cpp-trung.vim cpp-trung.vim
+ln -s $CP_DIR/vim/syntax/cxx.template cxx.template
+ln -s $CP_DIR/vim/syntax/cu.vim cu.vim
+# ln -s $CP_DIR/vim/.vimrc ~/.vimrc
+ln -s $CP_DIR/vim/.vimrc_version_YouCompleteMe ~/.vimrc
+ln -s $CP_DIR/vim/syntax/TEMPLATE.cpp TEMPLATE.cpp
