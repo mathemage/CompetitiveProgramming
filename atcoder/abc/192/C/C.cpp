6e0f63c8adf53d1901a830a1c82191cc79fa8391
Karel Ha <mathemage@gmail.com>
2021-04-23 21:51:28 +0200

    AtCoder ABC 192 C - Kaprekar Number
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/192/C/C.cpp
@@ -0,0 +1,187 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: C
+   LANG: C++14
+
+   * File Name : C.cpp
+   * Creation Date : 23-04-2021
+   * Last Modified : Fri 23 Apr 2021 09:51:20 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://atcoder.jp/contests/abc192/tasks/abc192_c
+   * Points/Time : 6m40s
+   * Total/ETA : 8m
+   * Status : AC
+   ==========================================*/
+
+#include <string>
+#define PROBLEMNAME "C"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) os << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const set<T>& vec) { 
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os; 
+} 
+
+template<typename T>
+ostream& operator<<(ostream& os, const multiset<T>& vec) {
+  os << "{ | ";
+  for (const auto & x: vec) os << x << "| ";
+  os << "}";
+  return os;
+}
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) {
+  for (auto & x: vec) is >> x;
+  return is;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {-1, 0, 1,  0};
+const vector<int> DY4 = { 0, 1, 0, -1};
+const vector<pair<int,int>> DXY4 = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+ll N,K;
+
+void solve() {
+  cin >> N >> K;
+
+  REP(_,K) {
+    string s1=to_string(N);
+    sort(REVALL(s1));
+
+    string s2=s1;
+    reverse(ALL(s2));
+
+    N=stoll(s1)-stoll(s2);
+  }
+
+  cout << N << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
