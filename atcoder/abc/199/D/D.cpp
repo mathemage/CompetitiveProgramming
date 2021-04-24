959d911096f831e087324bf750f8947246437ce6
Karel Ha <mathemage@gmail.com>
2021-04-24 15:47:21 +0200

    Compete in AtCoder ABC 199
    standings: 2658/8634 (page 133/536)
    -> PERCENTILE >69% :-/ (COVID-19, bad emotional state)
    rank: 2657/8716
    -> PERCENTILE >69% :-/ (COVID-19, bad emotional state)
    rating: +79 -> 618 (provisional) [7 KYU]
    performance as 962 :-/ ^_^
    
    Analysis:
    B:
    - pre-submit bug: <- typo:
      - `result &= (A[j]<=i && i<=B[j]);` instead of `ok &= (A[j]<=i && i<=B[j]);`
      <- automated reflex (similar solution to a previously solved problem)
      => calm down & read properly variable names when things don't work
    - 1st WA: <- typo:
      - forgotten debug version of for-loop: i up to 10 instead of 1000
      => add TODO next time in the commented part and check for all TODOs
      before submitting
      => skim through commented code (only debug prints should be there)
    
    C:
    - pre-submit bug: <- off-by-1 bug:
      - post-decremented indices `... -1;`
      - instead of pre-decremented indices `--Ai; --Bi;`
      <- forgotten 1-based indexing in the problem statement
      => pay attention to 1-based indexing in the problem statement
      -> silent segfault (stopping for loop)
      => look out to 1-based indexing bugs
      => look out to problem with indices when memory is manipulated and
      loops suddenly stops
    
    D:
    - impossible to finish in time when 3^20>1e9
    - read that 3-coloring is NP-complete
      -> hinted at brute-force / complete search solution, but couldn't find
      way to optimize search
    - noticed too late the observation:
      - 3 ways for first vertex
      - every vertices connected in the component choose only from <=2 remaining options
      => simplify brute-force analysis / complete search time complexity, to
      as best bound as possible
      => too low bounds -> hint for BF
    - too late to submit
    - WA anyway :-/ :-( -> to upsolve
    
    would have been 2384/8634 w/o the WA (typo) in B
    -> PERCENTILE >72% :-/
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/199/D/D.cpp
@@ -0,0 +1,269 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: D
+   LANG: C++14
+
+   * File Name : D.cpp
+   * Creation Date : 24-04-2021
+   * Last Modified : Sat 24 Apr 2021 03:42:04 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://atcoder.jp/contests/abc199/tasks/abc199_d
+   * Points/Time :
+   * Total/ETA : 400
+   * Status :
+   * [after contest] - too late :-/
+   * WA
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "D"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define MSG_VEC_PAIRS(v) print_vector_pairs((v), (#v));
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define MSG_VEC_PAIRS(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) {
+  os << endl;
+  for (const auto & s: vec) os << s << endl;
+  return os;
+}
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { 
+  for (const auto & x: vec) os << x << " ";
+  return os; 
+} 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { 
+  for (const auto & v: vec) os << v << endl;
+  return os; 
+} 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) {
+  for (const auto & x: vec) os << x << " ";
+  os << endl;
+  return os;
+} 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) {
+  for (const auto & x: vec) os << x << " ";
+  os << endl;
+  return os;
+}
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { 
+  for (const auto & x: vec) os << x.F << ":" << x.S << " | ";
+  return os; 
+} 
+
+template<typename T1, typename T2>
+void print_vector_pairs(const vector<pair<T1, T2>> & vec, const string & name) {
+  cerr << "> " << name << ": ";
+  for (const auto & x: vec) cerr << "(" << x.F << ", " << x.S << ")\t";
+  cerr << endl;
+}
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) {
+  for (auto & x: vec) is >> x;
+  return is;
+}
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {-1, 0, 1,  0};
+const vector<int> DY4 = { 0, 1, 0, -1};
+const vector<pair<int,int>> DXY4 = { {-1,0}, {0,1}, {1,0}, {0,-1} };
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+ll N,M;
+map<ll, vector<long long>> adj;
+vector<bool> vis;
+
+// set<map<ll,char>> solutions;
+string sol;
+set<string> solutions;
+
+ll compSize;
+ll df(ll at) {
+  ll result=0;
+  for (auto & neigh: adj[at]) {
+    if (!vis[neigh]) {
+      vis[neigh]=true;
+      result+=df(neigh);
+    }
+  }
+  return result;
+}
+
+string colors="RGB";
+
+ll nColored;
+void dfs(ll at) {
+  vis[at]=true;
+  nColored++;
+
+  for (auto & neigh: adj[at]) {
+    if (sol[neigh]=='_') {
+      for (auto & color: colors) {
+        if (color!=sol[at]) {
+          sol[neigh]=color;
+          dfs(neigh);
+          sol[neigh]='_';
+        }
+      }
+    } else if (sol[neigh]==sol[at]) {
+      return;
+    }
+  }
+
+  if (nColored==compSize) { // TODO
+    solutions.insert(sol);
+    return;
+  }
+}
+
+void solve() {
+  cin >> N >> M;
+
+  vector<ll> A(M); vector<ll> B(M);
+  REP(i,M) {
+    cin >> A[i] >> B[i];
+    A[i]--, B[i]--;
+    adj[A[i]].PB(B[i]);
+    adj[B[i]].PB(A[i]);
+  }
+  MSG(A); MSG(B);
+  MSG(adj);
+
+  ll result = 0LL;
+
+  vis.assign(N, false);
+  map<ll,ll> compSizes;
+  REP(at,N) {
+    compSizes[at]=df(at);
+  }
+
+  vis.assign(N, false);
+  REP(at,N) {
+    if (vis[at]) {
+      continue;
+    }
+    compSize=df(at);
+    sol.assign(N,'_');
+    sol[at]='R';
+    vis[at]=true;
+
+    nColored=0;
+    solutions.clear();
+    solutions.insert(sol);
+    dfs(at);
+
+    ll ways=3*solutions.size();
+    if (result==0) {
+      result=ways;
+    } else {
+      result*=ways;
+    }
+  }
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
