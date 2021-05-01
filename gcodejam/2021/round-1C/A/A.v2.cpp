2a52f1e3acb1f62b2670b1eabfdfeb2f31bb16e1
Karel Ha <mathemage@gmail.com>
2021-05-01 13:32:48 +0200

    Compete in Google CodeJam '21 Round 1C
    points: 40/100 pts
    penalty time: 2:31:45
    rank (in review): 2937/5899 (>8000 contestants submitted something)
    -> PERCENTILE (in review): >50% (but >2000 (1/4) contestants got 0 pts)
    -> PERCENTILE (in review): >63% of 8000 :-(
    
    Round overview:
    "Around 8000 coders submitted solutions in today's contest."
    
    "Ultimately, 71 coders managed a perfect score."
    
    "The unofficial cutoff to advance to Round 2 is 54 points."
    
    Analysis:
    - a lot of brute force solutions
      - this round was more about speedy (BF) solutions
    - could have done A directly for full point (but easy quick-win BF)
    - can't figure out C (TS1 - brute force)
      - although hinch for shortest path (graph) problem
    
    A:
    - 1833 (23%) failed TS1
    -  699 ( 9%) solved TS1
    - 4850 (61%) solved TS2
    - crazy last minute submission <- bug:
      - copy-paste typo *max_element instead of *min_element
    
    B:
    - 1399 (18%) failed TS1
    - 3204 (40%) solved TS1
    -  548 ( 7%) solved TS2
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gcodejam/2021/round-1C/A/A.v2.cpp
@@ -0,0 +1,199 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: A.v2
+   LANG: C++14
+
+   * File Name : A.v2.cpp
+   * Creation Date : 01-05-2021
+   * Last Modified : Sat 01 May 2021 01:28:20 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL :
+   * Points/Time :
+   * Total/ETA :
+   * Status :
+   * S AC AC !!!!!!!!!! (last minute !!!!!!!)
+   ==========================================*/
+
+#include <algorithm>
+#define PROBLEMNAME "A.v2"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+ll N,K;
+
+void solve() {
+  cin >> N >> K;
+  vector<int> P(N); cin >> P;
+  MSG(P);
+
+  set<ll> vals;
+  for (auto & Pi: P) { vals.insert(Pi); }
+  MSG(vals);
+//   vals.insert(0);
+//   vals.insert(K+1);
+
+  ll maxWins=0;
+
+  vector<ll> gapsHalves;
+  ll lastVal=UNDEF;
+  ll g;
+  for (auto & v: vals) {
+    if (lastVal!=UNDEF) {
+      g=max(0LL,v-lastVal-1LL);
+      MAXUPDATE(maxWins, g);
+      gapsHalves.PB((g+1LL)/2LL);
+    }
+    lastVal=v;
+  }
+  MSG(gapsHalves);
+  MSG(maxWins);
+  LINESEP1;
+
+  MAXUPDATE(maxWins, max(0LL,*min_element(ALL(P))-1LL));
+  MAXUPDATE(maxWins, max(0LL,K-*max_element(ALL(P))));
+  MSG(maxWins);
+  LINESEP1;
+
+  vector<long long> gaps=gapsHalves;
+  gaps.PB(max(0LL,*min_element(ALL(P))-1LL));
+  gaps.PB(max(0LL,K-*max_element(ALL(P))));
+
+  sort(REVALL(gaps));
+  if (SZ(gaps)>=2) {
+    MAXUPDATE(maxWins, gaps[0]+gaps[1]);
+  }
+  MSG(gaps);
+  MSG(maxWins);
+  LINESEP1;
+
+
+  double result = double(maxWins)/double(K);
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
---
+++ gcodejam/2021/round-1C/B/B.cpp
@@ -0,0 +1,190 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: B
+   LANG: C++14
+
+   * File Name : B.cpp
+   * Creation Date : 01-05-2021
+   * Last Modified : Sat 01 May 2021 01:34:52 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/codejam/round/00000000004362d7/00000000007c0f01
+   * Points/Time :
+   *
+   * Total/ETA : 15+20 pts
+   * Status :
+   * S AC ?
+   * S AC TLE (as expected :-/)
+   *
+   ==========================================*/
+
+#include <string>
+#include <unordered_set>
+#define PROBLEMNAME "B"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+ll Y;
+
+inline bool bigger(string A, string B) {
+  return SZ(A)>SZ(B) || (SZ(A)==SZ(B) && A>B);
+}
+
+void solve() {
+  string result(100,'9');
+  cin >> Y;
+
+  string Ystr=to_string(Y);
+  int Ylen=SZ(Ystr);
+
+  string roarStr;
+  FOR(k,1,Y) {
+    MSG(k);
+    if (SZ(to_string(k))+SZ(to_string(k+1))>Ylen+1) { break; }
+
+    roarStr="";
+    ll kk=k;
+    while (SZ(roarStr) < Ylen) {
+      roarStr+=to_string(kk);
+      kk++;
+    }
+
+    if (bigger(roarStr,Ystr) && bigger(result, roarStr)) {
+      result=roarStr;
+    } else {                  // one after
+      roarStr+=to_string(kk);
+
+      if (bigger(roarStr,Ystr) && bigger(result, roarStr)) {
+        result=roarStr;
+      }
+    }
+  }
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
