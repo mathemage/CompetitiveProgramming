ed079570eb85f66f8155a303ff92276f446085a6
Karel Ha <mathemage@gmail.com>
2021-05-08 15:41:25 +0200

    Compete in AtCoder ABC 200
    standings: 1193/8500 (page 60/484)
    -> PERCENTILE >85% ^_^
    rank: 1192/8577
    -> PERCENTILE >86%
    rating: +170 -> 788 (provisional) [7 KYU]
    performance as 1416 ! (=turquise :-O)
    
    Analysis:
    - came ~1m late because of Headspace meditation
      - faster by 1 min:
      -> probably would have rating >=800 [6 KYU] - green ! :-/
    => ALWAYS CAME ON TIME!
    => SET AN ALARM 1-2 MIN BEFORE!
    
    B:
    - simple simulation
    - hesitation with time complexity
      - vague idea that after each append, /= reduces the length back -> no ll overflow
    
    C:
    - good idea with reducing to modulo classes
    - also useful for the next problems
    => ALWAYS THINK OF SIMPLIFICATIONS!
    
    D:
    - DP-like implementation (~subset sum, but modulo)
    - pre-submit bug:
      <- confusing initial design
         - seqOfSeqs should be v<v<v<i>>>, not v<v<i>>
         -> mysterious compilation errors
    => THINK THOROUGHLY ABOUT DS AND THEIR DATA TYPES!
    - got confused from DS content when incomplete implementation
    => Don't hesitate/get confused when not fully implemented yet!
    - but finally AC (flash)!!! Yes ^_^ !
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/200/D/D.cpp
@@ -0,0 +1,194 @@
+ 
+/* ========================================
+
+   ID: mathema6
+   TASK: D
+   LANG: C++14
+
+   * File Name : D.cpp
+   * Creation Date : 08-05-2021
+   * Last Modified : So 8. května 2021, 15:14:37
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://atcoder.jp/contests/abc200/tasks/abc200_d
+   * Points/Time :
+   * Total/ETA : 400
+   * Status : AC !!!!!!!!!!!
+   ==========================================*/
+
+#define PROBLEMNAME "D"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  int N; cin >> N;
+  vector<int> A(N);
+  REP(i,N) {
+    cin >> A[i];
+    A[i]%=200;
+  }
+  MSG(A); LINESEP1;
+
+  bool check = false;
+  vector<vector<int>> result(2);
+
+  vector<vector<vector<int>>> seqOfSeqs(200);
+  REP(i,N) {
+    vector<pair<int, vector<int>>> newSeqs;
+    newSeqs.PB({A[i], {i+1}});
+
+    REP(modSum,200) {
+      for (auto & seq: seqOfSeqs[modSum]) {
+        vector<int> newSeq=seq; newSeq.PB(i+1);
+        newSeqs.PB({ (modSum+A[i])%200, newSeq });
+      }
+    }
+    MSG(newSeqs); LINESEP1;
+
+    for (auto & modSeq: newSeqs) {
+      int modSum=modSeq.F;
+      vector<int> seq=modSeq.S;
+
+      seqOfSeqs[modSum].PB(seq);
+      if (SZ(seqOfSeqs[modSum]) >= 2) {
+        result[0]=seqOfSeqs[modSum][0];
+        result[1]=seqOfSeqs[modSum][1];
+        check=true;
+        break;
+      }
+    }
+
+    if (check) { break; }
+  }
+
+  cout << (check?"Yes":"No") << endl;
+  if (check) {
+    for (auto & seq: result) {
+      cout << SZ(seq) << " " << seq << endl;
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
