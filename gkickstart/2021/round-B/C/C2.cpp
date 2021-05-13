cf57b8dd97934efca70ca217dd93de495770fb48
Karel Ha <mathemage@gmail.com>
2021-05-13 19:12:49 +0200

    Upsolve GKickStart '21B, C. Consecutive Primes
    - upsolved - 15m/[~12m ETA]
    S AC AC AC (finally passed TS3 !!)
    
    Analysis:
    - needed [editorial] to figure out
      <- hardcore number theory (maximal prime gaps, segmented sieves) :-O
      - 1389 participants (19%) solved 3 testsets
    - WA #1
      - forgot that is_prime(2)==true
      -> (just checked divisibility by 2, forgot the prime itself)
    => DON'T FORGET TO INCLUDE THE PRIME DIVISOR ITSELF AS THE PRIME!
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gkickstart/2021/round-B/C/C2.cpp
@@ -0,0 +1,195 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: C2
+   LANG: C++14
+
+   * File Name : C2.cpp
+   * Creation Date : 13-05-2021
+   * Last Modified : Thu 13 May 2021 07:12:45 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/kickstart/round/0000000000435a5b/000000000077a8e6
+   * Points/Time :
+   *      = 13m :-O :-/
+   * + 2m = 15m
+   *
+   * Total/ETA :
+   * [upsolve] ~12m
+   *
+   * Status :
+   * [editorial] <- hardcore number theory (maximal prime gaps, segmented sieves) :-O
+   * S WA -  -
+   * S AC AC AC (finally passed TS3 !!)
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "C2"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+ll Z;
+const ll MAX_GAP=282;
+
+inline bool is_prime(ll k) {
+  if (k%2==0) {
+    return k==2;
+  }
+
+  for (ll d = 3; d*d <= k; d += 2) {
+    if (k%d==0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void solve() {
+  cin >> Z;
+  MSG(Z);
+
+  ll ZZ=(ll)sqrt(Z);
+  MSG(ZZ);
+
+  ll result = 0LL;
+  ll lastPrime=UNDEF;
+  FOR(p, max(0LL, ZZ-2*MAX_GAP-1), ZZ+MAX_GAP+1) {
+    if (is_prime(p)) {
+      if (lastPrime!=UNDEF && p*lastPrime<=Z) {
+        MAXUPDATE(result, p*lastPrime);
+      }
+      lastPrime=p;
+    }
+  }
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
