aac970df97ab1829dc945af92fa260896e7134cc
Karel Ha <mathemage@gmail.com>
2021-05-18 21:16:00 +0200

    GKickStart '20C, B. Stable Wall
    S WA - :-/
    S AC AC !!!!!!! :-/
    
    Analysis:
    - dependency graph (when reading out columns)
      -> topological sort
    - struggled with implementation
    - map<char, bool> uses `false` by default
    - pre-submit bugs:
      - swapped usage of chTop and chBot
        - chTop was prereq of chBot
        <- originally named chStart, chEnd
    => NAME VARIABLES AS PRECISELY (ACCORDING TO THEIR FUNCTION/USAGE) AS POSSIBLE!
      - originally omitted to used `letters` altogether
        - just looping over the map
        - neglects the letters already w/o prereqs (i.e. source nodes in toposort)
    
    - WA on TS1
      - incorrect created `set<char> letters`
        - from traversal of pairs (chTop, chBot) in the column
        <- fails for R==1
    => ALWAYS CONSIDER EVEN THE EDGE CASES DURING IMPLEMENTATION!
    => ESPECIALLY WHEN CONSIDERING ALL DIFFERENT PAIRS 1<=I<J<=N, WHEN N CAN BE 1
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gkickstart/2020/round-C/B/B.cpp
@@ -0,0 +1,214 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: B
+   LANG: C++14
+
+   * File Name : B.cpp
+   * Creation Date : 18-05-2021
+   * Last Modified : Tue 18 May 2021 09:15:57 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff43/00000000003379bb
+   * Points/Time :
+   *      = 43m
+   * + 7m = 50m
+   *
+   * Total/ETA : 8+13pts
+   * Status :
+   * S WA - :-/
+   * S AC AC !!!!!!! :-/
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "B"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  ll R,C; cin >> R >> C;
+  vector<string> wall(R); cin >> wall;
+  MSG(wall); LINESEP1;
+
+  set<char> letters;
+  for (auto & line: wall) {
+    for (auto & ch: line) {
+      letters.insert(ch);
+    }
+  }
+
+  map<char, set<char>> prereq;
+  REP(c,C) {
+    FORD(rBot,R-1,1) {
+      FORD(rTop,rBot-1,0) {
+        char chTop=wall[rTop][c];
+        char chBot=wall[rBot][c];
+
+        if (chTop!=chBot) { prereq[chTop].insert(chBot); }
+      }
+    }
+  }
+//   for (auto & chPrereqs: prereq) { MSG(chPrereqs.F); MSG(chPrereqs.S); }
+//   LINESEP1;
+
+  map<char, bool> used;
+
+  int N=SZ(letters);
+  string result;
+  while (SZ(result)<N) {
+    char chFound='?';
+    for (auto & ch: letters) {
+      if (!used[ch] && SZ(prereq[ch])==0) {
+        chFound=ch;
+        break;
+      }
+    }
+    MSG(chFound);
+
+    if (chFound!='?') {
+      result+=chFound;
+      used[chFound]=true;
+
+      prereq.erase(chFound);
+      for (auto & it: prereq) {
+        it.S.erase(chFound);
+      }
+    } else {
+      cout << -1 << endl;
+      return;
+    }
+
+    MSG(result);
+    LINESEP1;
+  }
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
