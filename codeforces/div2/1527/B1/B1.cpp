1f61473f629d17d676b8d07d3819e5b21a2d9b1c
Karel Ha <mathemage@gmail.com>
2021-05-20 18:39:20 +0200

    Compete in Codeforces 721 Div 2
    successful hacks: 0/0
    failed systests: 0 :-)
    
    place in the division: 2044/15698 (15698 competitors)
    PERCENTILE: >86%
    (above some blue and purple coders!)
    
    rating: +74 -> 1450 [SPECIALIST!!] (rank 2044)
    
    Analysis:
    A:
    - took a moment
      - but submitted after 14 min
      - and passed pretests
    - the most significant bit must be zeroed out by (0111...1)_2
      - on the way, the AND_SUM encounters (1000...0)_2 -> guaranteed to
        zero out
    => HAVE THE MOST/LEAST SIGNIFICANT BITS IN MIND (FOR BIT MANIPULATION
    PROBLEMS)!
    
    B1:
    - when no middle 0
      - Bob can copy/mirror Alice's moves
      - until the last pair of (palindromic) 0's remains
        - then reduce to the first (sample) test case
      -> Bob has always lower cost
    - unless there's middle 0
      - Alice switches this middle 0 to 1
      - thus reducing to the case above
        - while Bob becomes the starting player (hence losing)
    - incomplete case analysis -> WA (on #3 - see B1.v1.cpp)
      <- missed case when there's ONLY the middle 0
         - Alice incurs a cost +1, while Bob doesn't even start to play
           - hence Bob wins with score 0
    => PAY MORE ATTENTION TO EDGE CASES!
      => if there's a single exception, what happens in the corner case?
      (single mid 0)
    => ALWAYS HANDLE/CATCH WITH APPROPRIATE TESTCASE
    
    C:
    - saw prefix sum after some hard thinking
    - combinatorial change of perspective:
      - in how many subsegments does pair {i1,i2} (for a_i1==a_i2) occur?
      - #pos before i1 (head) * # pos after i2 (tail)
    - head can be efficiently computed and updated via prefixSum of heads
      - idea from/similar to GKS20C, C. Perf. Subarray
        https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff43/00000000003381cb
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div2/1527/B1/B1.cpp
@@ -0,0 +1,164 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: B1
+   LANG: C++14
+
+   * File Name : B1.cpp
+   * Creation Date : 20-05-2021
+   * Last Modified : Thu 20 May 2021 06:38:48 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/contest/1527/problem/B1
+   * Points/Time : 523 [00:59]
+   * Total/ETA : 750
+   * Status :
+   * WA #3 :-(
+   * AC pretests
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "B1"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  ll n; cin >> n;
+  string s; cin >> s;
+
+  ll zs=0;
+  for (auto & c: s) {
+    zs+=(c=='0');
+  }
+
+  bool result = (n>1 && n%2 && s[n/2]=='0' && zs>1);
+  cout << (result?"ALICE":"BOB") << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
