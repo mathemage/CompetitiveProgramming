44d63758f50b5be79fd006c58db66b05e5e38511
Karel Ha <mathemage@gmail.com>
2021-05-23 16:12:07 +0200

    Compete in Google Kickstart '21 Round C
    points: 42/100 pts ^_^ <- 42 pts :-) :-D
    rank: 766/9065 (~12200 contestants)
    PERCENTILE: >91% (~ >93% overall)
    
    Round overview:
    N/A
    
    Analysis:
    A: S AC AC [0:35:57 +0]
    - tricky:
      - where's the middle index iMid?
      - how to build `pali`?
        - i.e. size of string::substr
    - +very tricky edge case
      - extra handling (see `pali<S` in the code)
    - pre-submit bug
      - count of available choices per position as `pos=S[i]-'a';`
      - instead of `pos=S[i]-'a'+1;`
    - but flashed on 1st pass: S AC AC ^_^
    
    => it always pays to:
      - DESIGN ALGO/APPROACH WELL/CLEARLY
        - anticipate potential pitfalls/problems/off-by-1's/unclear points
      - IMPLEMENT CAREFULLY (HANDLING EDGE CASES IN ADVANCE)
      - AND TEST THOROUGHLY (EXPLICITLY ADDING CASES WHERE IT MIGH FAIL)!
    
    B: S AC AC [1:12:47 +0]
    - got lost in simplificatons of arithmetic progression
      - concluded all to the way to a quadratic equation
      - misled:
        - thought the solutions can be {0,1,2,4}
        - depending on the equation's discriminant
    - step back for quick-win brute force for TS1
      - loop over L (count of progression)
      - but didn't work
    - pre-submit bug
      - RHS=G-(L*(L-1LL)/2LL);
      - inc result also for RHS<=0
        - didn't match with sample output
        -> noticed that K should be a POSITIVE integer
    => ALWAYS CHECK CAREFULLY FOR CONDITIONS/CONSTRAINTS OF THE DEFINED VARIABLES
       => AS SOON AS THERE STH UNCLEAR, FIND THE ANSWER IN THE STATEMENT!
    -> simplifies the solution
       -> early stop/break when RHS<=0
       - increasing L -> decreasing RHS => correct
    -> in the end this helped with the TS2, too!!!!
       - surprise pass (flash): S AC AC
    
    C: S AC WA - [1:52:45 +0]
    - task w/ randomized strategy
    - attacked w/ various heuristics
      - 8 competitive attempts
      - 1 practice attempts
      - only the 1st submission really worked
        - greedy (countering opp's most likely choice)
    
    D:
    - out of my reach
      - 323 (3%) failed
      -  56 (1%) passed TS1
      -  79 (1%) passed TS2
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gkickstart/2021/round-C/B/B.cpp
@@ -0,0 +1,173 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: B
+   LANG: C++14
+
+   * File Name : B.cpp
+   * Creation Date : 23-05-2021
+   * Last Modified : Sun 23 May 2021 02:13:28 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/kickstart/round/0000000000435c44/00000000007ec1cb
+   * Points/Time :
+   * Total/ETA : 7+12 pts
+   * Status : S AC AC !!!!!!!!!!!!!!!!!!!!!!!
+   ==========================================*/
+
+#define PROBLEMNAME "B"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define MINUPDATE(A,B) A = min((A), (B));
+#define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define YN(b) cout << ((b)?"YES":"NO") << endl;
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  ll G; cin >> G;
+  MSG(G);
+
+  ll result = 0LL;
+
+  ll RHS;
+  FOR(L,1,G) {
+    RHS=G-(L*(L-1LL)/2LL);
+    if (RHS<=0) {
+      break;
+    }
+
+    if (RHS%L==0) {
+      result++;
+      LINESEP1; MSG(RHS); MSG(L); MSG(RHS/L); MSG(result);
+    }
+  }
+
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
