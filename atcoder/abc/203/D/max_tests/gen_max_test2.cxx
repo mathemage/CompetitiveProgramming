3916300b8762802049383a11120348332281fcbf
Karel Ha <mathemage@gmail.com>
2021-05-30 15:41:49 +0200

    Compete in AtCoder ABC 203
    standings: 3136/8341 (~10400 competitors) on page 157/521
    -> PERCENTILE >62% (~ >69%) :-/ :-( <- stupid problem D
    rank: 3135/8432
    -> PERCENTILE >62% :-/ :-(
    rating: -20 -> 915 (provisional) [6 KYU] :-( :-(
    performance as 806 :-(
    - I came on time/in advance!!!
    - What could I have done better????! Problem D sucks!
    
    Analysis:
    - tested new Vim <F5> shortcut -> yank file content -> clipboard
      -> superfaster!
    - first 2 problems in <6 min
    - first 3 problems in <25 min
    - got stuck on 4th problem -> submitted at 1:34:32 (i.e. ~70min :-()
      -> better total time overall (compared to ABC 201)
    - 3/4 ACs, 1/4 WA/TLE :-/ :-(
    
    A:
    - hesitations: started implementing with A, B, C variable
      - but changed to vector<ll> and compare via abc[i]
    => DECIDE IN ADVANCE ON A,B,C... VARIABLES VS VEC OF VARS!!
    - quite lengthy solution for A :-/
      -> editorial?
    
    B:
    - original idea with string manip + stoll
    - faster via 100*n+k
    => CONSIDER BUILDING INTEGERS DIRECTLY!
       - before `string manip + stoll`
    
    C:
    - simulation w/ skipping intermediate steps
      - directly to end positions (i.e. towns w/ friends)
    - presubmit bug -> slowed down :-/
      - forgot to update `curPos=AB.F;`
    => WHILE RE-READING, CHECK EACH VARIABLE/STATE GETS UPDATED!!
      - especially in loops
      - `if (steps<0)` instead of `if (steps>K)`
    => THINK HARD ABOUT IF CONDITIONS / THE LOGIC OF THE ALGO!!!
    
    D:
    - sliding window
      - compute median via ordered_set in O(log K^2)
        - policy-based data structure available in gcc
      - but updates take O(K) * O(log K^2)
        - per each of O((N-K)^2) window
        -> amounts to worse than O(N^3) solution :-/ :-(
    - likely wrong idea/design
      -> to upsolve!
    - but only 775 ACs /3218 submissions (~24%)!
      - genuinely hard/tricky?
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/203/D/max_tests/gen_max_test2.cxx
@@ -0,0 +1,52 @@
+ 
+/* ========================================
+
+   ID: mathema6
+   TASK: gen_max_test
+   LANG: C++11
+
+   * File Name : gen_max_test.cxx
+   * Creation Date : 07-01-2021
+   * Last Modified : Sun 30 May 2021 03:07:23 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL :
+   * Points :
+   * Total :
+   * Status :
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define FOR(I,A,B) for(int I = (A); I < (B); ++I)
+#define RFOR(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define REP(I,N)   FOR(I,0,N)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define MSG(a) cerr << #a << " == " << (a) << endl;
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+
+void solve() {
+  long long result = 0LL;
+  cout << result << endl;
+}
+
+int main() {
+  // n, m...
+  int N=800, K=400;
+  cout << N << " " << K << endl;
+
+  int i=0;
+  REP(r,N) {
+    REP(c,N) {
+      cout << r+c << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
