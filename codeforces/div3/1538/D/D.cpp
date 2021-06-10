fb487aedfd6d9d17098b431917708c9969e8b3f8
Karel Ha <mathemage@gmail.com>
2021-06-10 18:51:47 +0200

    Compete in Codeforces 725 Div 3
    failed systests: 0 :-)
    
    place in the division: 3377/14168 (~16303 competitors)
    PERCENTILE: >76% (>~79%)
    
    rating: -24 -> 1427 [SPECIALIST] (rank 3377)
    
    Analysis:
    [skipped]
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div3/1538/D/D.cpp
@@ -0,0 +1,232 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: D
+   LANG: C++14
+
+   * File Name : D.cpp
+   * Creation Date : 10-06-2021
+   * Last Modified : Thu 10 Jun 2021 06:50:30 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/contest/1538/problem/D
+   * Points/Time :
+   * Total/ETA :
+   * Status : unsolved after 1hr+ of trying :-( :-( :-( :-(
+   ==========================================*/
+
+#define PROBLEMNAME "D"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+// #define MINUPDATE(A,B) A = min((A), (B));
+// #define MAXUPDATE(A,B) A = max((A), (B));
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define YN(b) cout << ((b)?"YES":"NO") << endl;
+// #define YN(b) cout << ((b)?"Yes":"No") << endl;
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+const int MAX_PR = 5e4+100;
+bool isprime[MAX_PR];
+
+vector<int> eratosthenes_sieve(int LIMIT) {
+	memset(isprime, 1, sizeof(isprime));
+	isprime[0]=isprime[1]=false;
+	for(int i=2;i*i<LIMIT;++i)
+		if(isprime[i])
+			for(int j=i*i;j<LIMIT;j+=i)
+				isprime[j]=false;
+	vector<int> pr;
+	FOR(i,2,LIMIT) if(isprime[i]) pr.push_back(i);
+	return pr;
+}
+
+vector<int> primes;
+
+void solve() {
+  ll a,b,k; cin >> a >> b >> k;
+  MSG(a); MSG(b); LINESEP1;
+
+  vector<long long> alphaA(SZ(primes));
+  vector<long long> alphaB(SZ(primes));
+
+  if (k==1) {
+    YN(a==b);
+    return;
+  }
+
+  ll minK=0LL;
+  ll maxSingles=0LL;
+  for (int i = 0; i < SZ(primes); i += 1) {
+    ll p=primes[i];
+
+    while (a%p==0) {
+      a/=p;
+      alphaA[i]++;
+    }
+
+    while (b%p==0) {
+      b/=p;
+      alphaB[i]++;
+    }
+
+    minK+=alphaA[i]!=alphaB[i];
+    maxSingles+=abs(alphaA[i]-alphaB[i]);
+  }
+  MSG(minK); MSG(maxSingles); LINESEP1;
+//   MSG(alphaA); MSG(alphaB); LINESEP1;
+
+  MSG(k);
+  if (k<minK) {
+    NO;
+    return;
+  }
+
+  if (k<=maxSingles) {
+    YES;
+    return;
+  } else {
+    NO;
+  }
+
+  k-=maxSingles;
+
+//   bool result = minK<=k && k<=maxK;
+//   YN(result);
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout << std::setprecision(10) << std::fixed;
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  primes = eratosthenes_sieve(MAX_PR);
+//   primes = eratosthenes_sieve(20);
+  MSG(SZ(primes));
+  LINESEP2;
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
