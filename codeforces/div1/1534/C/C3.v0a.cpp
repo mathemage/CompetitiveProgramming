3a7eba2d27ecacfe6072f8b38977b43508db0e71
Karel Ha <mathemage@gmail.com>
2021-06-18 00:28:03 +0200

    Upsolve Codeforces LATOKEN Round 1, problem C. Little Alawn's Puzzle
    AC !!!! YESSSSSS! Union-Find forever!! -> early bed ^_^ [flash in = 11m50s]
    AC too!!
    
    Analysis:
    coding kata 2:
    - union-find
      - no need for compSizes
        -> interested in nComps
    
    coding kata 3:
    - update links directly in the unite()
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ codeforces/div1/1534/C/C3.v0a.cpp
@@ -0,0 +1,203 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: C3
+   LANG: C++14
+
+   * File Name : C3.cpp
+   * Creation Date : 18-06-2021
+   * Last Modified : Fri 18 Jun 2021 12:25:26 AM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codeforces.com/contest/1534/problem/C
+   * Points/Time :
+   *
+   * Total/ETA :
+   * [coding kata 2 - via union-find] ~12m
+   *
+   * Status :
+   ==========================================*/
+
+#define PROBLEMNAME "C3"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define YN(b) cout << ((b)?"YES":"NO") << endl;
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+void solve() {
+  ll n; cin >> n;
+  map<ll, vector<long long>> edges;
+  ll val;
+  REP(_,2) {
+    REP(pos,n) {
+      cin >> val;
+      edges[val].PB(pos);
+    }
+  }
+
+  vector<int> link(n+5); iota(ALL(link), 0);
+  vector<ll> sizes(n+5);
+
+  function<int(int)> find = [&](int x) {
+    return x==link[x] ? x : link[x]=find(link[x]);
+  };
+  auto unite = [&](int a, int b) {
+    int ra=find(a);
+    int rb=find(b);
+    // TODO swap sizes
+    sizes[ra]+=sizes[rb];
+    link[rb]=ra;
+  };
+
+  FOR(val,1,n) {
+    if (SZ(edges[val]) == 2) {
+      unite(edges[val][0], edges[val][1]);
+    } else {
+      assert(SZ(edges[val]) == 2);
+    }
+  }
+
+  // product of comp sizes
+  vector<long long> compSizes;
+  REP(pos,n) {
+    if (find(pos)==pos) {
+      compSizes.PB(sizes[pos]);
+    }
+  }
+
+  ll result = accumulate(ALL(compSizes), 0LL, [](auto & a, auto & b) { return a=(a+b%MOD)%MOD; } );
+  cout << result << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout << std::setprecision(10) << std::fixed;
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
