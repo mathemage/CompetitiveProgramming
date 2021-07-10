fa9c0b598ff73033752b52ab79d99d0935fc7557
Karel Ha <mathemage@gmail.com>
2021-07-10 15:39:37 +0200

    Compete in AtCoder ABC 209
    standings: 2873/8675 (~10700 competitors) on p. 144/536
    -> PERCENTILE >66% (~ >73%) :-/ :-(
    rank: 2871/8758
    -> PERCENTILE >67% :-/ :-/
    rating: -14 -> 984 (provisional) [6 KYU] :-/
    performance as 891 :-/ [green - 4th worst in history :-/]
    - stupid pattern mining :-( :-(
    
    Analysis
    --------
    - Take-it-easy round (chill & relax before GKS tomorrow 7:00-10:00 :-/ )
    - I came on time.
    - first 2 problems in <6 min
    - first 3 problems in <28 min
    - cryptic 4th problem (seemingly LCA, but 2-coloring via BFS was enough)
      - submitted at 0:78:53 (i.e. ~50min :-/ :-/ )
        - could have been faster (w/o pattern mining)
    - 4/4 ACs, but slow time :-/
    
    A:
    - hesitations:
      - weird wording "not {less,more} than"
        - confusing
      - intuition, no thorough thinking about case A>B
        - rely on rectifying via `max(0,.)`
      - fastest solution in 15s
        - I guess no copy-paste samples
    => REMEMBER: "NOT {LESS,MORE} THAN" MEANS {>=, <=}!
    => think hard through wordings to understanding the problem statement
    
    B:
    - traverse and decrement at odd indices (i.e. i%2!=0)
    - hesitations
      - how to incorporate the odd-index updates
      - ternary `?:` vs `if`
    => USE `IF` WHEN UNSURE!
       - LEADS TO CLEARER/MORE STRAIGHTFORWARD CODE
       - ALLOWS FOR POSSIBLE ELSE (IF) BRANCHES
    
    C:
    - tricky to figure out
      - tried to look for DP desperately
      - or some complex/hard combinatorics
    - enough to count number of variations
      - presort C[] first
        - order of A[i] sequence doesn't matter
        - least options first, then more and more options
        - at (0-based) position `i`, already `i` values have been used
          -> subtract `i` from number of options
        - multiple to result and modulo
    => BEWARE OF PATTERN MATCHING!!
       - AIM TO SOLVE THE PROBLEM (C-D) AS SIMPLY AS POSSIBLY
       - USING AS MUCH COMMON SENSE AS POSSIBLE
       - ONLY THEN RESORT TO SPECIALIZED ALGOS
    
    D:
    - cryptic problem
      - seemingly LCA
        - get dist() via BFS/DFS depths minus depth of LCA
        - then use parity of dist() to decide the result
      - but 2-coloring via BFS was enough
        - same color/parity? => Town
        - diff color/parity? => Road
        - in the end very simple code via BFS & checking 2-colors
    - lost a lot of time learning / trying to understand LCA
    => STOP & PONDER!!!!!!!! PAUSE & (RE-)PLAN!!!!!!!!
       - IF STH SEEMS TOO COMPLEX (FROM READING ON THE INTERNET), THEN THERE
         MUST BE A SIMPLER WAY!!!
    - lost a lot of copy-pasting & trying to understand LCA from KACTL
      codebook
    => BE SUSPICIOUS ABOUT USING CODEBOOKS/ADVANCED KACTL FOR ABC C-D PROBLEMS!!
    => BEWARE OF PATTERN MATCHING!!!!
       - AIM TO SOLVE THE PROBLEM (C-D) AS SIMPLY AS POSSIBLY!
       - USING AS MUCH COMMON SENSE AS POSSIBLE!!
       - ONLY THEN RESORT TO SPECIALIZED ALGOS!
    => PREFER TO RE-DESIGN TO SIMPLER CODE, IF IT'S REALLY AN OVERKILL
       - WORTH IT
       - BUT SAVE THE OLD CODE!
    
    E:
    - unsolved
      <- out of my reach
    - digraphs?
      - build explicitly?
      -> not enough time! O(N^2) ~ 1e10 :-/
    - toposort?
      - and then DP?
    - only 247 ACs / 1080 submissions
      - must be hardcore++++
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ atcoder/abc/209/D/D.cpp
@@ -0,0 +1,245 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: D
+   LANG: C++14
+
+   * File Name : D.cpp
+   * Creation Date : 10-07-2021
+   * Last Modified : Sat 10 Jul 2021 03:12:18 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL :
+   * Points/Time :
+   * Total/ETA :
+   * Status :
+   ==========================================*/
+
+#define PROBLEMNAME "D"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((int) (x).size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define YN(b) cout << ((b)?"YES":"NO") << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define Yn(b) cout << ((b)?"Yes":"No") << endl;
+
+using ll = long long;
+using ul = unsigned long long;
+using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+using ld = long double;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const unordered_map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+using vi=vector<int>;
+
+template<class T>
+struct RMQ {
+	vector<vector<T>> jmp;
+	RMQ(const vector<T>& V) : jmp(1, V) {
+		for (int pw = 1, k = 1; pw * 2 <= SZ(V); pw *= 2, ++k) {
+			jmp.emplace_back(SZ(V) - pw * 2 + 1);
+			FOR(j,0,SZ(jmp[k]))
+				jmp[k][j] = min(jmp[k - 1][j], jmp[k - 1][j + pw]);
+		}
+	}
+	T query(int a, int b) {
+		assert(a < b); // or return inf if a == b
+		int dep = 31 - __builtin_clz(b - a);
+		return min(jmp[dep][a], jmp[dep][b - (1 << dep)]);
+	}
+};
+
+struct LCA {
+	int T = 0;
+	vi time, path, ret;
+	RMQ<int> rmq;
+
+	LCA(vector<vi>& C) : time(SZ(C)), rmq((dfs(C,0,-1), ret)) {}
+
+	void dfs(vector<vi>& C, int v, int par) {
+		time[v] = T++;
+		for (int y : C[v]) if (y != par) {
+			path.push_back(v), ret.push_back(time[v]);
+			dfs(C, y, v);
+		}
+	}
+
+	int lca(int a, int b) {
+		if (a == b) return a;
+		tie(a, b) = minmax(time[a], time[b]);
+		return path[rmq.query(a, b)];
+	}
+
+};
+
+void solve() {
+  ll N,Q; cin >> N >> Q;
+
+  vector<vector<int>> adj(N);
+  ll ai, bi;
+  REP(_,N-1) {
+    cin >> ai >> bi;
+    --ai, --bi;
+
+    adj[ai].PB(bi);
+    adj[bi].PB(ai);
+  }
+
+  LCA lca(adj);
+
+  vector<int> depth;
+	auto dfs = [&](int v, int depth) {
+    for (auto & it: neig) {
+
+    }
+		for (int y : C[v]) if (y != par) {
+			path.push_back(v), ret.push_back(time[v]);
+			dfs(C, y, v);
+		}
+	}
+
+  ll ci, di;
+  REP(_,Q) {
+    cin >> ci >> di;
+    --ci, --di;
+    MSG(ci); MSG(di);
+
+// 	dist(a,b){return depth[a] + depth[b] - 2*depth[lca(a,b)];}
+    ll dist=UNDEF;
+    MSG(dist);
+    cout << ((dist%2)?"Road":"Town") << endl;
+
+    LINESEP1;
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout << std::setprecision(10) << std::fixed;
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
+
