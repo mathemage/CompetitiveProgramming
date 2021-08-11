209a7b4e5f88e800ffa3f6969dba6b47f342a166
Karel Ha <mathemage@gmail.com>
2021-08-11 23:39:51 +0200

    TopCoder SRM-809-Div-1-250: LunchLine
    +15-30m reading & thinking
    216.12/250 [AC!!] <- yes, 1st Div 1 Easy practice!
    241.04/250 [AC!]  <- coding kata 2
    
    Analysis
    --------
    
    - hesitations/slow down
      - `id2qi[i]=i;` vs. `std::iota(ALL(.), 0);`
      - initialize `int qi=N;`
        - or `int qi=N-1;`?
    => ALWAYS PREFER CLEARER CODE!!
       => EVEN IF IT MEANS LONGER CODE!
    
    - presubmit bug
      - forgotten `i++;` when accumulating result
        -> multiply by 0 -> returns 0 :-/
    
    - coding kata 2
      - presubmit bug
        - `i++` instead of `++i` -> all testcases failed :-/
    => USE PRE-/POST-INCREMENT AS A SEPARATE COMMAND!
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/SRM-809-DIV-1/250/LunchLine.cc
@@ -0,0 +1,169 @@
+/* ========================================
+   * Created By : mathemage
+   * Points/Time : 241.04
+   * Total/ETA : 250
+   * Status : AC!
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define OP first
+#define CL second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((ll) (x).size())
+#define SZi(x) ((int) (x).size())
+#define YES return "Possible";
+#define NO return "Impossible";
+#define YN(b) return ((b)?"Possible":"Impossible");
+
+using ll = long long;
+using ul = unsigned long long;
+// using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+using ld = long double;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define _D(a) cerr << "> " << (#a) << ": "; cerr <<= (a); cerr << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n"; (print_vector_vector(cerr, (v))) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+
+  #define MSG_VEC(v) cerr << "> " << (#v) << ": "; (print_vector(cerr, (v))) << endl;
+  #define MSG_VEC_STR(v) cerr << "> " << (#v) << ": "; (print_vector_string(cerr, (v))) << endl;
+#else
+  #define MSG(a)
+  #define _D(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+
+  #define MSG_VEC(v)
+  #define MSG_VEC_STR(v)
+#endif
+
+template<typename T>
+inline ostream& operator<<=(ostream& os, const T & x) { os << x; return os; }
+
+inline ostream& operator<<=(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) cerr << s << endl; return os; }
+inline ostream& print_vector_string(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) cerr << s << endl; return os; }
+
+template<typename T>
+inline ostream& operator<<=(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; }
+template<typename T> 
+inline ostream& print_vector(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T>
+inline ostream& print_vector_vector(ostream& os, const vector<vector<T>> & vec) {
+  for (const auto & line: vec) {
+    for (const auto & it: line) {
+      os << it << " ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+inline ostream& operator<<=(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T, class Compare>
+inline ostream& operator<<=(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const unordered_map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const vector<pair<T1, T2>>& vec) { for (const auto & x: vec) {os <<= x; os << " ";} return os; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+class LunchLine {
+public:
+  long long simulate(int N, int M, int A, int B, int C, int D, int E) {
+    vector<ll> K(M);
+    if (M>0) { K[0]=A; }
+    if (M>1) { K[1]=B; }
+    FOR(i,2,M-1) { K[i] = (C*K[i-1] + D*K[i-2] + E) % N; }
+
+    vector<int> id2qi(N);
+    map<int, int> qi2id;
+    REP(i,N) { qi2id[i]=id2qi[i]=i; }
+
+    ll qi=N;
+    REP(i,M) {
+      qi2id.erase(id2qi[K[i]]);
+      qi2id[qi]=K[i];
+      id2qi[K[i]]=qi++;
+    }
+
+    ll result=0;
+    ll i=0;
+    for (auto & it: qi2id) {
+      result+= i++ * it.S;
+    }
+    return result;
+  }
+};
+
+/*
+* (RE-)COMPILE LAST VERSION BEFORE SUBMITTING!!!!!
+  - or CHECK ADDITIONALLY IN THE ROOM SUMMARY (whether CORRECT source code was submitted)!!!!
+
+* Be careful of INT OVERFLOW WHEN INPUT PARAMETERS ARE IN INTS!
+  - re-cast to ll vars
+*/
