b80f0d44a804fc9b96f554ad98ba01d28fc2d8f8
Karel Ha <mathemage@gmail.com>
2021-08-17 23:58:31 +0200

    GKickStart '20E, B. High Buildings
    S WA - [37m] :-/ :-(
    S WA - [54m] :-( :-(
    S AC AC !!! yes!!! [+16m23s] - B2.v1.cpp
    
    Analysis
    --------
    
    v2 (B2.v1.cpp)
    - order:
      - segment of second highest
        <- (A-C)-times
      - segment of highest
        <- ~(C/2)-times
      - segment of lowest (i.e. 1)
        <- (N-A-B+C)-times
      - segment of highest
        <- ~(C/2)-times
      - segment of second highest
        <- (B-C)-times
    - extra checks/guards for impossibility:
      - too many visible buildings
      - A==B==C==1 but N!=1
    - presubmit bugs:
      - when (A-C==0 && C/2==0), e.g. sample #1
        - then the left observer can see the segment of lowest buildings directly
        -> would count towards A!
        -> `ll leftOne = (A-C==0 && C/2==0);`
           - use leftOne to control whether upper/lower half of C/2 goes to left or right
        => REMEMBER THINK ABOUT CASE WHEN X==1 -> X/2==0!
    
    v2B (B2.v2.cpp)
    - based on editorial solution
      - "hide" segment of lowest buildings somewhere
        - where we're guaranteed to have higher buildings around!
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ gkickstart/2020/round-E/B/B2.v1.cpp
@@ -0,0 +1,196 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: B2
+   LANG: C++14
+
+   * File Name : B2.cpp
+   * Creation Date : 17-08-2021
+   * Last Modified : Tue 17 Aug 2021 11:58:13 PM CEST
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : https://codingcompetitions.withgoogle.com/kickstart/round/000000000019ff47/00000000003bef73#problem
+   * Points/Time :
+   * = 16m :-/
+   *
+   * Total/ETA : 15m
+   * Status :
+   * S AC AC !!! yes!!!
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "B2"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define OP first
+#define CL second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((ll) (x).size())
+#define SZi(x) ((int) (x).size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define YN(b) cout << ((b)?"YES":"NO") << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define Yn(b) cout << ((b)?"Yes":"No") << endl;
+
+using ll = long long;
+using ul = unsigned long long;
+// using llll = pair<ll, ll>;
+using ulul = pair<ul, ul>;
+using ld = long double;
+using graph_unord = unordered_map<ll, vector<ll>>;
+using graph_ord  = map<ll, set<ll>>;
+using graph_t    = graph_unord;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const unordered_map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2> 
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+const ld PI = acos((ld)-1);
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+
+void solve() {
+  ll N,A,B,C; cin >> N >> A >> B >> C;
+  MSG(N); MSG(A); MSG(B); MSG(C); LINESEP1;
+
+  if (A+B-C>N) {
+    cout << "IMPOSSIBLE" << endl;
+    return;
+  }
+  if (max({A,B,C})==1 && N>1) {
+    cout << "IMPOSSIBLE" << endl;
+    return;
+  }
+
+  ll leftOne = (A-C==0 && C/2==0);
+
+  cout << vector<int>(A-C,N-1);
+  cout << vector<int>((C+leftOne)/2,N);
+  cout << vector<int>(N-A-B+C,1);
+  cout << vector<int>((C+1-leftOne)/2,N);
+  cout << vector<int>(B-C,N-1);
+  cout << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout << std::setprecision(10) << std::fixed;
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  cin >> cases;
+  FOR(tt,1,cases) {
+    cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
