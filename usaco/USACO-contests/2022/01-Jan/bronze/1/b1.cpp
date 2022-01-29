9ee33f555245224506f26751b03475a6cd072dbe
Karel Ha <mathemage@gmail.com>
2022-01-29 17:51:11 +0100

    Compete in USACO 2022 January, Bronze
    points: 1000/1000 pts (flashed all 3/3 problems perfectly!!)
    => IN-CONTEST PROMOTION
    (ICPed)!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Yes, so happy now
    ^_^
    
    Round overview:
    "The bronze division had 6935 total participants, of whom 5382 were
    pre-college students. All competitors who scored 750 or higher on this
    contest are automatically promoted to the silver division.
    
    Your score on this contest was 1000. Each problem contributed 1000/3
    possible points, with equal points assigned to each test case; you can
    recall your performance on each test case by clicking on a problem below
    and looking at your results in analysis mode."
    
    Analysis
    --------
    (retrospective)
    
    * Problem 1. Herdle
      - 11/11 ACs (passed & flashed!)
      - loop over grid to count greens
        - deduct from counters
      - loop over counters
        - take min to account for yellows
    
    * Problem 2. Non-Transitive Dice
      - 11/11 ACs (passed & flashed again!! ^_^)
      - brute-force/exhaustive search
        - `if (B>C && C>A) {` -> found the die
      - tricks:
        - `using die = vector<ll>;`
        - custom comparator `bool operator>(die X, die Y)`
        - `if (B>A) { swap(A, B); }`
    
    * Problem 3. Drought
      - trickiest problem
        - a lot of presubmit bugs & debugging
        - almost lost hope :-( => but ICP!!! yes!!!
      - 15/15 ACs (passed & flashed!!) -> ICPed!!!
      - presubmit bugs:
        - `+=` instead of `-=` offset `minOff`
      - tricks:
        - when index `i` outside of scope `d[i]`, just use 0 (neutral element for addition)
          - via custom lambda getter
        - compute dummy `d[N-1]` (even though not valid)
          - forces the final constraint (at the last position) via computing and offseting by `minOff`
          - but then don't accumulate into the result (only d[i] for i=0..N-2)
        - reversing `h` -> recompute results -> do they agree?
          - should deal w/ odd vs. even/off-by-1 annoyances
      -> luckily finally worked => passed & flashed => ICP!!! Yes!!!
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ usaco/USACO-contests/2022/01-Jan/bronze/1/b1.cpp
@@ -0,0 +1,225 @@
+/* ========================================
+
+   ID: mathema6
+   TASK: b1
+   LANG: C++14
+
+   * File Name : b1.cpp
+   * Creation Date : 29-01-2022
+   * Last Modified : Sat 29 Jan 2022 03:02:58 PM CET
+   * Created By : Karel Ha <mathemage@gmail.com>
+   * URL : http://usaco.org/index.php?page=viewproblem&cpid=1167
+   * Points/Time :
+   * Total/ETA :
+   * Status :
+   * 11/11 ACs (passed & flashed!)
+   *
+   ==========================================*/
+
+#define PROBLEMNAME "b1"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl "\n"
+// #define endl "\n" << flush  // flush for interactive problems
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define OP first
+#define CL second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((ll) (x).size())
+// #define SZi(x) ((int) (x).size())
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define YN(b) cout << ((b)?"YES":"NO") << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define Yn(b) cout << ((b)?"Yes":"No") << endl;
+#define Imp cout << "Impossible" << endl;
+#define IMP cout << "IMPOSSIBLE" << endl;
+
+using ll = long long;
+using ul = unsigned long long;
+// using ulul = pair<ul, ul>;
+using ld = long double;
+using graph_unord = unordered_map<ll, vector<ll>>;
+using graph_ord  = map<ll, set<ll>>;
+using graph_t    = graph_unord;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n" << (v) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+#else
+  #define MSG(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+#endif
+
+ostream& operator<<(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) os << s << endl; return os; }
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T> 
+ostream& operator<<(ostream& os, const vector<vector<T>> & vec) { for (const auto & v: vec) os << v << endl; return os; } 
+
+template<typename T>
+inline ostream& operator<<(ostream& os, const vector<vector<vector<T>>> & vec) {
+  for (const auto & row: vec) {
+    for (const auto & col: row) {
+      os << "[ " << col << "]   ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; } 
+
+template<typename T, class Compare>
+ostream& operator<<(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const unordered_map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T>
+istream& operator>>(istream& is, vector<T> & vec) { for (auto & x: vec) is >> x; return is; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+// inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+inline bool eqDouble(ld a, ld b) { return fabs(a-b) < 1e-9; }
+inline bool isCollinear(ld x, ld y, ld x1, ld y1, ld x2, ld y2) {
+  // (x-x1)/(y-y1) == (x-x2)/(y-y2)
+  // (x-x1)*(y-y2) == (x-x2)*(y-y1)
+  return eqDouble((x-x1)*(y-y2), (x-x2)*(y-y1));
+}
+inline bool isBetween(ld x, ld y, ld x1, ld y1, ld x2, ld y2) {
+  return min(x1,x2)<=x && x<=max(x1,x2)
+      && min(y1,y2)<=y && y<=max(y1,y2);
+}
+inline bool onLine(ld x, ld y, ld x1, ld y1, ld x2, ld y2) {
+  return isCollinear(x, y, x1, y1, x2, y2) && isBetween(x, y, x1, y1, x2, y2);
+}
+
+
+#ifndef MATHEMAGE_LOCAL
+void setIO(string filename) {    // the argument is the filename without the extension
+	freopen((filename+".in").c_str(), "r", stdin);
+	freopen((filename+".out").c_str(), "w", stdout);
+  MSG(filename);
+}
+#endif
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const long long MOD = 1000000007;
+const double EPS = 1e-8;
+const ld PI = acos((ld)-1);
+
+const int INF = INT_MAX;
+const long long INF_LL = LLONG_MAX;
+const long long INF_ULL = ULLONG_MAX;
+
+
+void solve() {
+  vector<string> ans(3); cin >> ans;
+  vector<string> guess(3); cin >> guess;
+  MSG(ans); MSG(guess); LINESEP1;
+  
+  unordered_map<char, ll> ansFreq;
+  for (auto & line: ans) {
+    for (auto & ch: line) {
+      ansFreq[ch]++;
+    }
+  }
+  unordered_map<char, ll> guessFreq;
+  for (auto & line: guess) {
+    for (auto & ch: line) {
+      guessFreq[ch]++;
+    }
+  }
+  MSG(ansFreq); MSG(guessFreq); LINESEP1;
+
+  ll green = 0LL;
+  REP(r,3) {
+    REP(c,3) {
+      if (ans[r][c]==guess[r][c]) {
+        green++;
+        ansFreq[ans[r][c]]--;
+        guessFreq[guess[r][c]]--;
+      }
+    }
+  }
+
+  ll yellow = 0LL;
+  for (auto & [ch, cnt]: ansFreq) {
+    yellow += min(cnt, guessFreq[ch]);
+  }
+
+  cout << green << endl;
+  cout << yellow << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout << std::setprecision(10) << std::fixed;
+
+#ifndef MATHEMAGE_LOCAL
+//   setIO(PROBLEMNAME);
+#endif
+
+  int cases = 1;
+  FOR(tt,1,cases) {
+//     cout << "Case #" << tt << ": ";
+    solve();
+    LINESEP2;
+  }
+  return 0;
+}
