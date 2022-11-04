d4f7762bda4c7e1d17116d1cb78406a9e2d0533a
Karel Ha <mathemage@gmail.com>
2022-11-04 19:17:39 +0100

    TopCoder Rookie-SRM-15-Div-1-1000: Spelling
    553.27/1000 WA :-/
    409.19/1000 AC!!! (finally)
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ topcoder/SRM/Rookie-SRM-15-DIV-1/1000/Spelling.v0b.cc
@@ -0,0 +1,214 @@
+/* ========================================
+   * Created By : mathemage
+   * Points/Time :
+   * Total/ETA : 1000
+   * Status :
+   ==========================================*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define REP(i,n) for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) for(ll i=(a);i<=(b);i++)
+#define FORD(i,a,b) for(ll i=(a);i>=(b);i--)
+#define REPi(i,n) for(int i=0;i<(n);i++)
+#define FORi(i,a,b) for(int i=(a);i<=(b);i++)
+#define FORDi(i,a,b) for(int i=(a);i>=(b);i--)
+#define ALL(A)     (A).begin(), (A).end()
+#define REVALL(A)     (A).rbegin(), (A).rend()
+#define F first
+#define S second
+#define OP first
+#define CL second
+#define PB push_back
+#define MP make_pair
+#define MTP make_tuple
+#define SGN(X) ((X) ? ( (X)>0?1:-1 ) : 0)
+#define CONTAINS(S,E) ((S).find(E) != (S).end())
+#define SZ(x) ((ll) (x).size())
+#define YES return "Possible";
+#define NO return "Impossible";
+#define YN(b) return ((b)?"Possible":"Impossible");
+
+using ll = long long;
+using ul = unsigned long long;
+using ld = long double;
+using graph_unord = unordered_map<ll, vector<ll>>;
+using graph_ord  = map<ll, set<ll>>;
+using graph_t    = graph_unord;
+
+#ifdef ONLINE_JUDGE
+  #undef MATHEMAGE_DEBUG
+#endif
+
+#ifdef MATHEMAGE_DEBUG
+  #define MSG(a) cerr << "> " << (#a) << ": " << (a) << endl;
+  #define _D(a) cerr << "> " << (#a) << ": "; cerr <<= (a); cerr << endl;
+  #define MSG_VEC_VEC(v) cerr << "> " << (#v) << ":\n"; (print_vector_vector(cerr, (v))) << endl;
+  #define LINESEP1 cerr << "-----------------------------------------------                  " << endl;
+  #define LINESEP2 cerr << "_________________________________________________________________" << endl;
+  #define LINESEP3 cerr << "#################################################################" << endl;
+
+  #define MSG_VEC(v) cerr << "> " << (#v) << ": "; (print_vector(cerr, (v))) << endl;
+  #define MSG_VEC_STR(v) cerr << "> " << (#v) << ": "; (print_vector_string(cerr, (v))) << endl;
+#else
+  #define MSG(a)
+  #define _D(a)
+  #define MSG_VEC_VEC(v)
+  #define LINESEP1
+  #define LINESEP2
+  #define LINESEP3
+
+  #define MSG_VEC(v)
+  #define MSG_VEC_STR(v)
+#endif
+
+template<typename T>
+inline ostream& operator<<=(ostream& os, const T & x) { os << x; return os; }
+
+inline ostream& operator<<=(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) cerr << s << endl; return os; }
+inline ostream& print_vector_string(ostream& os, const vector<string> & vec) { os << endl; for (const auto & s: vec) cerr << s << endl; return os; }
+
+template<typename T>
+inline ostream& operator<<=(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; }
+template<typename T> 
+inline ostream& print_vector(ostream& os, const vector<T> & vec) { for (const auto & x: vec) os << x << " "; return os; } 
+
+template<typename T>
+inline ostream& print_vector_vector(ostream& os, const vector<vector<T>> & vec) {
+  for (const auto & line: vec) {
+    for (const auto & it: line) {
+      os << it << " ";
+    }
+    os << endl;
+  }
+  return os;
+}
+
+template<typename T, class Compare>
+inline ostream& operator<<=(ostream& os, const set<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T, class Compare>
+inline ostream& operator<<=(ostream& os, const multiset<T, Compare>& vec) { for (const auto & x: vec) os << x << " "; os << endl; return os; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const unordered_map<T1, T2>& vec) { for (const auto & x: vec) os << x.F << ":" << x.S << " | "; return os; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const pair<T1, T2>& p) { return os << "(" << p.F << ", " << p.S << ")"; }
+
+template<typename T1, typename T2>
+inline ostream& operator<<=(ostream& os, const vector<pair<T1, T2>>& vec) { for (const auto & x: vec) {os <<= x; os << " ";} return os; }
+
+template<typename T>
+inline bool bounded(const T & x, const T & u, const T & l=0) { return min(l,u)<=x && x<max(l,u); }
+
+template<class T> bool umin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool umax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+// inline bool eqDouble(double a, double b) { return fabs(a-b) < 1e-9; }
+inline bool eqDouble(ld a, ld b) { return fabs(a-b) < 1e-9; }
+inline bool isCollinear(ld x, ld y, ld x1, ld y1, ld x2, ld y2) {
+  // (x-x1)/(y-y1) == (x-x2)/(y-y2)
+  // (x-x1)*(y-y2) == (x-x2)*(y-y1)
+  return eqDouble((x-x1)*(y-y2), (x-x2)*(y-y1));
+}
+inline bool isBetween(ld x, ld y, ld x1, ld y1, ld x2, ld y2) {
+  return min(x1,x2)<=x && x<=max(x1,x2)
+      && min(y1,y2)<=y && y<=max(y1,y2);
+}
+inline bool onLine(ld x, ld y, ld x1, ld y1, ld x2, ld y2) {
+  return isCollinear(x, y, x1, y1, x2, y2) && isBetween(x, y, x1, y1, x2, y2);
+}
+
+
+const vector<int> DX8 = {-1, -1, -1,   0, 0,   1,  1,  1};
+const vector<int> DY8 = {-1,  0,  1,  -1, 1,  -1,  0,  1};
+const vector<pair<int,int>> DXY8 = {
+  {-1,-1}, {-1,0}, {-1,1},
+  { 0,-1},         { 0,1},
+  { 1,-1}, { 1,0}, { 1,1}
+};
+
+const vector<int> DX4 = {0, 1,  0, -1};
+const vector<int> DY4 = {1, 0, -1,  0};
+const vector<pair<int,int>> DXY4 = { {0,1}, {1,0}, {0,-1}, {-1,0} };
+const string dues="NESW";
+
+const int CLEAN = -1;
+const int UNDEF = -42;
+const ll MOD = 1000000007;
+const double EPS = 1e-8;
+const ld PI = acos((ld)-1);
+
+const int INF = INT_MAX;
+const ll INF_LL = LLONG_MAX;
+const ll INF_ULL = ULLONG_MAX;
+
+class Spelling {
+public:
+  int distance(vector <string> dictionary, string word) {
+    LINESEP3;
+
+    auto getDist = [&](string s) {
+      string w = word;
+      int penalty = SZ(s)==SZ(w) ? 0 : 3;
+      if (SZ(s)<SZ(w)) {
+        swap(s,w);
+        penalty = 2;
+      }
+
+      if (abs(SZ(s)-SZ(w)) <= 1) {
+        vector<int> splits{INF};
+        if (SZ(s)>SZ(w)) {
+          FOR(split,0,SZ(s)) {
+            splits.PB(split);
+          }
+        }
+
+        int ans=INF;
+        for (auto & split: splits) {
+          int dist = split==INF ? 0 : penalty;
+          for (ll i = 0; i < SZ(w); i += 1) {
+            dist += (w[i] != s[i + (i>=split)]);
+          }
+          if (umin(ans, dist)) {
+            _D(penalty);
+            _D(s); _D(w);
+            _D(split);
+            _D(dist);
+            LINESEP1;
+          }
+        }
+
+        return ans;
+      } else {
+        return INF;
+      }
+    };
+
+    int result = INF;
+    for (auto & dic: dictionary) {
+      umin(result, getDist(dic));
+    }
+    return result==INF ? -1 : result;
+  }
+};
+
+/*
+* (RE-)COMPILE LAST VERSION BEFORE SUBMITTING!!!!!
+  - or CHECK ADDITIONALLY IN THE ROOM SUMMARY (whether CORRECT source code was submitted)!!!!
+
+* NEVER COPY-PASTE-OVERWRITE, ALWAYS WRITE FRESH/ANEW!!!!
+
+* ONLY COPY-PASTE WHEN DUPLICATING CODE W/O ANY MODIFICATIONS NEEDED!!
+
+* READ THE WHOLE CODE, EVERY LINE, WHEN CHECKING/DEBUGGING!!!
+
+* Be careful of INT OVERFLOW WHEN INPUT PARAMETERS ARE IN INTS!
+  - re-cast to ll vars
+*/
