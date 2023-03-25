4f488087a09941a89dbf009c16b57474c1c9dfe8
Karel Ha <karel.ha@tum.de>
2023-03-25 20:42:46 +0100

    AtCoder ABC 247B - Unique Nicknames
    WA -> WA -> testcases -> AC! :-/
    
    Signed-off-by: Karel Ha <karel.ha@tum.de>

---
+++ atcoder/abc/247/B/B.v1.py
@@ -0,0 +1,33 @@
+# https://atcoder.jp/contests/abc247/tasks/abc247_b
+# 14m +
+# WA -
+
+import logging
+
+# logging.basicConfig(level=logging.DEBUG)
+
+N = int(input())
+names = set()
+forbidden = set()
+
+s = [None] * N
+t = [None] * N
+for i in range(N):
+    s[i], t[i] = input().split()
+    logging.debug(f"s[i] == {s[i]}")
+    logging.debug(f"t[i] == {t[i]}")
+    logging.debug(f"_" * 80)
+
+    for w in [s[i], t[i]]:
+        if w in names:
+            forbidden.add(w)
+        else:
+            names.add(w)
+
+ok = True
+for i in range(N):
+    if s[i] in forbidden and t[i] in forbidden:
+        ok = False
+        break
+
+print("Yes" if ok else "No")
